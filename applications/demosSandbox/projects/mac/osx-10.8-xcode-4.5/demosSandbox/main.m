90b588d19e3a973649caab43b166bb1e1ee67e6f
Julio Jerez <jerezjulio0@gmail.com>
2013-05-12 03:13:42 +0000

    adding legitimate demosSanbox crated form Xcode 4.5 this create a bundle, display the main menu, and store the asset in the resource suborder inside the bundle

---
+++ applications/demosSandbox/projects/mac/osx-10.8-xcode-4.5/demosSandbox/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  demosSandbox
+//
+//  Created by Julio Jerez on 5/11/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
