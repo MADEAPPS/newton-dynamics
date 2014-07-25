674adff6bb63ddfc5c8caa9172e3bfd5b329c95e
JulioJerez <jerezjulio0@gmail.com>
2014-07-24 22:56:10 -0700

    set kinematic placement as default

--- applications/demosSandbox/sdkDemos/NewtonDemos.cpp
+++ applications/demosSandbox/sdkDemos/NewtonDemos.cpp
@@ -29,7 +29,7 @@
 //#define DEFAULT_SCENE	3			// Precessing tops
 //#define DEFAULT_SCENE	4			// closest distance
 //#define DEFAULT_SCENE	5			// primitive collision
-//#define DEFAULT_SCENE	6 			// Kinematic bodies
+#define DEFAULT_SCENE	6 			// Kinematic bodies
 //#define DEFAULT_SCENE	7			// primitive convex cast 
 //#define DEFAULT_SCENE	8			// Box stacks
 //#define DEFAULT_SCENE	9			// simple level mesh collision
@@ -54,7 +54,7 @@
 //#define DEFAULT_SCENE	28          // standard joints
 //#define DEFAULT_SCENE	29			// articulated joints
 //#define DEFAULT_SCENE	20			// basic rag doll
-#define DEFAULT_SCENE	31			// basic car
+//#define DEFAULT_SCENE	31			// basic car
 //#define DEFAULT_SCENE	32			// high performance super car
 //#define DEFAULT_SCENE	33			// basic player controller
 //#define DEFAULT_SCENE	34			// advanced player controller
