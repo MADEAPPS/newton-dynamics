8621c788640b3f9954928ce0cdb7edf6923b6664
JulioJerez <jerezjulio0@gmail.com>
2014-08-25 16:13:05 -0700

    more script work

--- packages/dCompilerKit/dLittleScriptLanguage/dDAGExpressionNodeAssigment.cpp
+++ packages/dCompilerKit/dLittleScriptLanguage/dDAGExpressionNodeAssigment.cpp
@@ -92,86 +92,17 @@ void dDAGExpressionNodeAssigment::CompileCIL(dCIL& cil)
 		dTree<dThreeAdressStmt::dArg, dString>::dTreeNode* const variable = dDAG::FindLocalVariable(m_leftVariable->m_name);
 		dAssert (variable);
 		dThreeAdressStmt::dArg arg1 (LoadLocalVariable(cil, variable->GetInfo()));
+		dThreeAdressStmt::dArg arg0 (LoadLocalVariable(cil, m_expression->m_result));
 
 		dAssert (m_parent);
 		// emit an indirect addressing mode
 		dThreeAdressStmt& tmp = cil.NewStatement()->GetInfo();
 		tmp.m_instruction = dThreeAdressStmt::m_store;
-		tmp.m_arg0.m_label = cil.NewTemp();
+		tmp.m_arg0 = arg0;
 		tmp.m_arg1 = arg1;
-		//tmp.m_arg2 = dimSize.m_arg0;
-		tmp.m_arg2.m_label = result;
+		tmp.m_arg2 = dim->m_result;
 		DTRACE_INTRUCTION (&tmp);
 		m_result = tmp.m_arg0; 
-
-/*
-		dString& variable (m_leftVariable->m_name);
-		int pos = variable.Find (D_SCOPE_PREFIX, 0, int (strlen (D_SCOPE_PREFIX)));
-		if (pos != 0) {
-			bool state = RenameLocalVariable(cil, variable);
-			if (!state) {
-				dTrace (("undefined local variable\n"));
-				dAssert (0);
-			}
-		}
-
-		dDAGDimensionNode* const dim = m_leftVariable->m_dimExpressions.GetFirst()->GetInfo();
-		dim->CompileCIL(cil);
-		dCIL::dListNode* const dimInstruction = cil.NewStatement();
-		dThreeAdressStmt& addressIndex = dimInstruction->GetInfo();
-		addressIndex.m_instruction = dThreeAdressStmt::m_assigment;
-		addressIndex.m_arg0.m_label = cil.NewTemp();
-		addressIndex.m_arg1 = dim->m_result; 
-
-		dString result = addressIndex.m_arg0.m_label;
-		DTRACE_INTRUCTION (&addressIndex);
-
-		for (dList<dDAGDimensionNode*>::dListNode* node = m_leftVariable->m_dimExpressions.GetFirst()->GetNext(); node; node = node->GetNext()) {
-			dAssert (0);
-			dDAGDimensionNode* const dim = node->GetInfo();
-			dim->CompileCIL(cil);
-			
-			dThreeAdressStmt& stmtMul = cil.NewStatement()->GetInfo();
-			stmtMul.m_instruction = dThreeAdressStmt::m_assigment;
-			stmtMul.m_operator = dThreeAdressStmt::m_mul;
-			stmtMul.m_arg0.m_label = cil.NewTemp();
-			stmtMul.m_arg1.m_label = result;
-			stmtMul.m_arg2.m_label = dim->m_arraySize;
-
-			DTRACE_INTRUCTION (&stmtMul);
-
-			dThreeAdressStmt& stmtAdd = cil.NewStatement()->GetInfo();
-			stmtAdd.m_instruction = dThreeAdressStmt::m_assigment;
-			stmtAdd.m_operator = dThreeAdressStmt::m_add;
-			stmtAdd.m_arg0.m_label = cil.NewTemp();
-			stmtAdd.m_arg1.m_label = stmtMul.m_arg0.m_label;
-			stmtAdd.m_arg2 = dim->m_result;
-
-			result = stmtAdd.m_arg0.m_label;
-
-			DTRACE_INTRUCTION (&stmtAdd);
-		}
-
-		dAssert (m_parent);
-		dThreeAdressStmt& dimSize = cil.NewStatement()->GetInfo();
-		dimSize.m_instruction = dThreeAdressStmt::m_assigment;
-		dimSize.m_operator = dThreeAdressStmt::m_mul;
-		dimSize.m_arg0.m_label = cil.NewTemp();
-		dimSize.m_arg1.m_label = result; 
-		dimSize.m_arg2.m_type = dThreeAdressStmt::m_intConst;
-		dimSize.m_arg2.m_label = "4"; 
-		DTRACE_INTRUCTION (&dimSize);
-
-			
-		// emit an indirect addressing mode
-		dThreeAdressStmt& tmp = cil.NewStatement()->GetInfo();
-		tmp.m_instruction = dThreeAdressStmt::m_store;
-		tmp.m_arg0 = m_expression->m_result;
-		tmp.m_arg1.m_label = variable;
-		tmp.m_arg2.m_label = dimSize.m_arg0.m_label;
-		DTRACE_INTRUCTION (&tmp);
-		m_result.m_label = tmp.m_arg0.m_label; 
-*/
 	} else {
 		m_leftVariable->CompileCIL(cil); 
 		dThreeAdressStmt::dArg arg1 (LoadLocalVariable(cil, m_expression->m_result));
