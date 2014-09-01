5682c704bc8c69d5af122400a226934551a61705
JulioJerez <jerezjulio0@gmail.com>
2014-09-01 16:44:57 -0700

    commented out vehicle debug code

--- applications/demosSandbox/sdkDemos/demos/BasicCar.cpp
+++ applications/demosSandbox/sdkDemos/demos/BasicCar.cpp
@@ -539,7 +539,7 @@ class BasicVehicleEntity: public DemoEntity
 		// check transmission type
 		int toggleTransmission = m_automaticTransmission.UpdateTriggerButton (mainWindow, 0x0d) ? 1 : 0;
 
-#if 1
+#if 0
 	#if 0
 		static FILE* file = fopen ("log.bin", "wb");                                         
 		if (file) {
