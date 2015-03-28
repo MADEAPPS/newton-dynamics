53ae4a9d2c45c2e6b5d5c640ee23f5ef478774e8
JulioJerez <jerezjulio0@gmail.com>
2015-03-28 10:37:51 -0700

    relax test for matrix orthogonality in SSE

--- coreLibrary_300/source/core/dgMatrix.h
+++ coreLibrary_300/source/core/dgMatrix.h
@@ -273,10 +273,10 @@ DG_INLINE bool dgMatrix::TestOrthogonal() const
 		   (m_up[3] == dgFloat32 (0.0f)) & 
 		   (m_right[3] == dgFloat32 (0.0f)) & 
 		   (m_posit[3] == dgFloat32 (1.0f)) &
-		   (dgAbsf(a - dgFloat32 (1.0f)) < dgFloat32 (1.0e-4f)) & 
-		   (dgAbsf(b - dgFloat32 (1.0f)) < dgFloat32 (1.0e-4f)) &
-		   (dgAbsf(c - dgFloat32 (1.0f)) < dgFloat32 (1.0e-4f)) &
-		   (dgAbsf(d - dgFloat32 (1.0f)) < dgFloat32 (1.0e-4f)); 
+		   (dgAbsf(a - dgFloat32 (1.0f)) < dgFloat32 (1.0e-3f)) & 
+		   (dgAbsf(b - dgFloat32 (1.0f)) < dgFloat32 (1.0e-3f)) &
+		   (dgAbsf(c - dgFloat32 (1.0f)) < dgFloat32 (1.0e-3f)) &
+		   (dgAbsf(d - dgFloat32 (1.0f)) < dgFloat32 (1.0e-3f)); 
 }
 
 DG_INLINE bool dgMatrix::TestSymetric3x3() const
