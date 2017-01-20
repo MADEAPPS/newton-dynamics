e20a9692bf499c3684f71bcde66040c8931af4ca
JulioJerez <jerezjulio0@gmail.com>
2017-01-20 09:16:37 -0800

    now start adding new spring mass solve that conside per particle masses.

--- coreLibrary_300/source/physics/dgCollisionMassSpringDamperSystem.cpp
+++ coreLibrary_300/source/physics/dgCollisionMassSpringDamperSystem.cpp
@@ -224,3 +224,165 @@ void dgCollisionMassSpringDamperSystem::CalculateAcceleration(dgFloat32 timestep
 		}
 	}
 }
+
+
+#if 0
+void dgCollisionMassSpringDamperSystem::CalculateAcceleration(dgFloat32 timestep)
+{
+	// Ks is in [sec^-2] a spring constant unit acceleration, not a spring force acceleration. 
+	// Kc is in [sec^-1] a damper constant unit velocity, not a damper force acceleration. 
+
+	dgInt32 iter = 4;
+	dgVector* const accel = &m_accel[0];
+	dgVector* const veloc = &m_veloc[0];
+	dgVector* const posit = &m_posit[0];
+	dgVector* const extAccel = &m_externalAccel[0];
+	const dgSpringDamperLink* const links = &m_linkList[0];
+/*
+	
+	dgVector* const dx = dgAlloca(dgVector, m_linksCount);
+	dgVector* const dv = dgAlloca(dgVector, m_linksCount);
+	dgVector* const dpdv = dgAlloca(dgVector, m_linksCount);
+
+	dgVector* const normalDir = dgAlloca(dgVector, m_particlesCount);
+	dgVector* const normalAccel = dgAlloca(dgVector, m_particlesCount);
+	dgFloat32* const spring_A01 = dgAlloca(dgFloat32, m_linksCount);
+	dgFloat32* const spring_B01 = dgAlloca(dgFloat32, m_linksCount);
+	dgFloat32* const frictionCoeffecient = dgAlloca(dgFloat32, m_particlesCount);
+*/
+	//	dgFloat32* const damper_A01 = dgAlloca(dgFloat32, m_linksCount);
+	//	dgFloat32* const damper_B01 = dgAlloca(dgFloat32, m_linksCount);
+	//	dgFloat32* const damper_C01 = dgAlloca(dgFloat32, m_linksCount);
+	//	dgVector* const collisionDir1 = dgAlloca(dgVector, m_particlesCount);
+	//	dgVector* const collisionDir2 = dgAlloca(dgVector, m_particlesCount);
+	//	dgVector* const tmp1 = dgAlloca(dgVector, m_particlesCount);
+	//	dgVector* const tmp2 = dgAlloca(dgVector, m_particlesCount);
+	//	dgVector* const diag = dgAlloca(dgVector, m_particlesCount);
+	//	dgVector* const offDiag = dgAlloca(dgVector, m_particlesCount);
+	//dgVector deltaOmega(m_body->m_invWorldInertiaMatrix.RotateVector(m_body->m_externalTorque.Scale4(timestep)));
+
+	dgWorld* const world = m_body->GetWorld();
+	world->m_solverJacobiansMemory.ResizeIfNecessary(GetMemoryBufferSizeInBytes() + 1024);
+
+	dgVector* const normalAccel = (dgVector*)&world->m_solverJacobiansMemory[0];
+	dgVector* const normalDir = &normalAccel[m_particlesCount];
+	dgFloat32* const frictionCoeffecient = (dgFloat32*)&normalDir[m_particlesCount];
+
+	m_body->m_alpha = dgVector::m_zero;
+	m_body->m_omega = dgVector::m_zero;
+	m_body->m_externalForce = dgVector::m_zero;
+	m_body->m_externalTorque = dgVector::m_zero;
+
+	dgVector unitAccel(m_body->m_externalForce.CompProduct4(m_body->m_invMass.m_w));
+
+	// here I need to add all other external acceleration like wind and pressure, friction and collision.
+	for (dgInt32 i = 0; i < m_particlesCount; i++) {
+		extAccel[i] = unitAccel;
+	}
+
+	//dgFloat32 ks_dt = -timestep * kSpring;
+	//dgFloat32 kd_dt0 = -timestep * kDamper;
+	//dgFloat32 kd_dt1 = -timestep * kDamper * dgFloat32(2.0f);
+
+	dgVector dtRK4(timestep / iter);
+	dgVector epsilon(dgFloat32(1.0e-14f));
+
+	dtRK4 = dtRK4 & dgVector::m_triplexMask;
+	HandleCollision(timestep, normalDir, normalAccel, frictionCoeffecient);
+	for (dgInt32 k = 0; k < iter; k++) {
+		for (dgInt32 i = 0; i < m_particlesCount; i++) {
+			accel[i] = dgVector::m_zero; 
+		}
+/*
+		for (dgInt32 i = 0; i < m_linksCount; i++) {
+			const dgInt32 j0 = links[i].m_m0;
+			const dgInt32 j1 = links[i].m_m1;
+			dv[i] = veloc[j0] - veloc[j1];
+			dx[i] = posit[j0] - posit[j1];
+
+			const dgVector p0p1(dx[i]);
+			const dgVector v0v1(dv[i]);
+			const dgVector length2(p0p1.DotProduct4(p0p1));
+			const dgVector mask(length2 > m_smallestLenght2);
+
+			const dgVector lenght2((length2 & mask) | length2.AndNot(mask));
+			const dgFloat32 length = (lenght2.Sqrt()).GetScalar();
+			const dgFloat32 den = dgFloat32(1.0f) / length;
+			const dgFloat32 lenghtRatio = links[i].m_restlength * den;
+			const dgFloat32 compression = dgFloat32(1.0f) - lenghtRatio;
+			const dgVector fs(p0p1.Scale4(links[i].m_spring * compression));
+			const dgVector fd(p0p1.Scale4(links[i].m_damper * den * den * (v0v1.DotProduct4(p0p1)).GetScalar()));
+
+			dgAssert(fs.m_w == dgFloat32(0.0f));
+			dgAssert(fs.m_w == dgFloat32(0.0f));
+			dgAssert(p0p1.m_w == dgFloat32(0.0f));
+
+			dpdv[i] = p0p1.CompProduct4(v0v1);
+			accel[j0] -= (fs + fd);
+			accel[j1] += (fs + fd);
+
+			dgFloat32 ks_dt = -dtRK4.GetScalar() * links[i].m_spring;
+			spring_A01[i] = ks_dt * compression;
+			spring_B01[i] = ks_dt * lenghtRatio * den * den;
+		}
+
+		for (dgInt32 i = 0; i < m_linksCount; i++) {
+			const dgVector dv0(dv[i]);
+			const dgVector A01(spring_A01[i]);
+			const dgVector B01(spring_B01[i]);
+			const dgVector dfdx(A01.CompProduct4(dv0) + B01.CompProduct4(dx[i].CompProduct4(dpdv[i])));
+
+			const dgInt32 j0 = links[i].m_m0;
+			const dgInt32 j1 = links[i].m_m1;
+
+			dgAssert(dfdx.m_w == dgFloat32(0.0f));
+			accel[j0] += dfdx;
+			accel[j1] -= dfdx;
+		}
+*/
+
+		for (dgInt32 i = 0; i < m_linksCount; i++) {
+			const dgInt32 j0 = links[i].m_m0;
+			const dgInt32 j1 = links[i].m_m1;
+			const dgVector p0p1(posit[j0] - posit[j1]);
+			const dgVector v0v1(veloc[j0] - veloc[j1]);
+			const dgVector dvdp(v0v1.CompProduct4(p0p1));
+
+			const dgVector p0p1Mag2(p0p1.DotProduct4(p0p1));
+			const dgVector p0p1Mask(p0p1Mag2 > m_smallestLenght2);
+			const dgVector p0p1Lenght2((p0p1Mag2 & p0p1Mask) | m_smallestLenght2.AndNot(p0p1Mask));
+
+			const dgFloat32 p0p1Length = p0p1Lenght2.Sqrt().GetScalar();
+			const dgFloat32 p0p1InvMag = dgFloat32 (1.0f) / p0p1Length;
+			const dgFloat32 k01 = links[i].m_spring * (p0p1Length - links[i].m_restlength) * p0p1InvMag;
+			const dgFloat32 d01 = links[i].m_damper * dvdp.GetScalar() * p0p1InvMag * p0p1InvMag;
+			const dgVector netForce (p0p1.Scale4(k01 + d01));
+
+			accel[j0] += netForce;
+			accel[j1] -= netForce;
+		}
+
+
+		for (dgInt32 i = 0; i < m_particlesCount; i++) {
+			//dgVector dirAccel1 (collisionDir1[i].CompProduct4(accel[i].DotProduct4(collisionDir1[i])));
+			//dgVector dirAccel2 (collisionDir2[i].CompProduct4(accel[i].DotProduct4(collisionDir2[i])));
+			//tmp0[i] = accel[i] + collidingAccel[i] - dirAccel0 - dirAccel1 - dirAccel2;
+
+			dgVector netAccel (accel[i] + extAccel[i]);
+			dgVector tangentDir(veloc[i] - normalDir[i].CompProduct4(normalDir[i].DotProduct4(veloc[i])));
+			dgVector mag(tangentDir.DotProduct4(tangentDir) + epsilon);
+			
+			dgFloat32 tangentFrictionAccel = dgAbsf(netAccel.DotProduct4(normalDir[i]).GetScalar());
+			dgVector friction(tangentDir.Scale4(frictionCoeffecient[i] * tangentFrictionAccel / dgSqrt(mag.GetScalar())));
+
+			//dgVector particleAccel (accel[i] + normalAccel[i] - normalDirAccel);
+			dgVector normalDirAccel(normalDir[i].CompProduct4(netAccel.DotProduct4(normalDir[i])));
+			//accel[i] = accel[i] + normalAccel[i] - normalDirAccel - friction;
+			netAccel = netAccel + normalAccel[i] - normalDirAccel - friction;
+			veloc[i] += netAccel.CompProduct4(dtRK4);
+			posit[i] += veloc[i].CompProduct4(dtRK4);
+
+		}
+	}
+}
+
