257a31013652df9e31c35092961f795ee37d79fc
NewtonDynamics <jerezjulio0@gmail.com>
2017-03-07 16:33:56 -0800

    found very important bug that may explain all the problem I had.
    still more work to do.

--- packages/dMath/dVector.h
+++ packages/dMath/dVector.h
@@ -112,14 +112,6 @@ class dBigVector: public TemplateVector<dFloat64>
 template<class T>
 TemplateVector<T>::TemplateVector() 
 {
-/*
-#ifdef _DEBUG
-	static int xxxx;
-	xxxx ++;
-	if (xxxx > 1024 * 10)
-		dAssert (0);
-#endif
-*/
 }
 
 template<class T>
