add0aa1c9de331b3e1e60df3fefef29e115fc73f
JulioJerez <jerezjulio0@gmail.com>
2017-03-11 06:53:14 -0800

    improve controller imputs an litle.

--- applications/demosSandbox/sdkDemos/demos/SuperCar.cpp
+++ applications/demosSandbox/sdkDemos/demos/SuperCar.cpp
@@ -105,8 +105,8 @@ static CarDefinition monsterTruck =
 	5200.0f,									// PEAK_HP_RPM
 	6000.0f,									// REDLINE_TORQUE_RPM
 	264.0f,										// VEHICLE_TOP_SPEED_KMH
-	1.0f,										// TIRE_LATERAL_STIFFNESS
-	1.0f,										// TIRE_LONGITUDINAL_STIFFNESS
+	0.5f,										// TIRE_LATERAL_STIFFNESS
+	0.5f,										// TIRE_LONGITUDINAL_STIFFNESS
 	0.5f,										// TIRE_ALIGNING_MOMENT_TRAIL
 	4000.0f,									// TIRE_SUSPENSION_SPRING
 	200.0f,										// TIRE_SUSPENSION_DAMPER
@@ -147,8 +147,8 @@ static CarDefinition viper =
 	5200.0f,									// PEAK_HP_RPM
 	6000.0f,									// REDLINE_TORQUE_RPM
 	264.0f,										// VEHICLE_TOP_SPEED_KMH
-	1.0f,										// TIRE_LATERAL_STIFFNESS
-	1.0f,										// TIRE_LONGITUDINAL_STIFFNESS
+	0.5f,										// TIRE_LATERAL_STIFFNESS
+	0.5f,										// TIRE_LONGITUDINAL_STIFFNESS
 	0.5f,										// TIRE_ALIGNING_MOMENT_TRAIL
 	30000.0f,									// TIRE_SUSPENSION_SPRING
 	700.0f,										// TIRE_SUSPENSION_DAMPER
@@ -625,7 +625,7 @@ class SuperCarEntity: public DemoEntity
 		}
 
 #if 0
-	#if 1
+	#if 0
 		static FILE* file = fopen ("log.bin", "wb");                                         
 		if (file) {
 			fwrite (&engineIgnitionKey, sizeof (int), 1, file);
@@ -695,6 +695,7 @@ class SuperCarEntity: public DemoEntity
 
 				case m_engineStop:
 				{
+					engine->SetClutchParam(0.0f);
 					if (forwardGasPedal || reverseGasPedal) {
 						brakes->SetParam(1.0f);
 					} else {
@@ -728,6 +729,9 @@ class SuperCarEntity: public DemoEntity
 							m_drivingState = m_engineStop;
 						}
 					} else {
+						if (handBrakePedal && (engine->GetSpeed() < 2.0f)) {
+							engine->SetClutchParam(0.0f);
+						}
 						brakes->SetParam(0.0f);
 					}
 
