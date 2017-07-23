df2e2b15cd5834d5412fcfbc6c6382e6ac579f08
JulioJerez <jerezjulio0@gmail.com>
2017-07-23 11:34:32 -0700

    forget to check demo file.

--- applications/demosSandbox/sdkDemos/demos/BasicRagdoll.cpp
+++ applications/demosSandbox/sdkDemos/demos/BasicRagdoll.cpp
@@ -71,7 +71,7 @@ static dPasiveRagDollDefinition skeletonRagDoll[] =
 {"Bip01_L_Thigh",    "capsule", one_dof,   0.0f, 90.0f,  0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f,  14.0f,	 -120.0f,  120.0f,  -120.0f, 120.0f, -60.0f, 60.0f,  0.0f,  -90.0f,  -0.0f},
 
 //	{"Bip01_L_Thigh",    "capsule", two_dof,   0.0f, 90.0f,  0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f,  14.0f,	 -30.0f,  30.0f,  -120.0f, 120.0f, -60.0f, 60.0f,  0.0f,  -90.0f,  -0.0f},
-//	{"Bip01_L_Calf",     "capsule", one_dof,   0.0f, 90.0f,  0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f,  10.0f,  -150.0f,  0.0f,     0.0f,	90.0f,   0.0f, -0.0f,  0.0f,    0.0f,  90.0f}, 
+	{"Bip01_L_Calf",     "capsule", one_dof,   0.0f, 90.0f,  0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f,  10.0f,  -5.0f,  140.0f,     0.0f,	90.0f,   0.0f, -0.0f,  0.0f,    0.0f,   0.0f}, 
 //	{"Bip01_L_Foot",  "convexhull", two_dof,   0.0f, 0.0f,   0.0f, 0.0f, 0.0f,  0.0f, 0.0f,  0.0f,   3.0f,      0.0f,  0.0f,   -45.0f,	45.0f,  -45.0f, 45.0f, 0.0f,   -90.0f,  -0.0f}, 
 
 //	{ "Bip01_R_Thigh",   "capsule", two_dof, 0.0f, 90.0f, 0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f, 14.0f, -30.0f, 30.0f, -120.0f, 120.0f, -60.0f, 60.0f, 0.0f, -90.0f, -0.0f },
