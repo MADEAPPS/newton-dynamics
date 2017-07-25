bd0f555eacc8bb8a7e642581a7a7152dc39a1a85
JulioJerez <jerezjulio0@gmail.com>
2017-07-25 08:19:10 -0700

    complete gaith with null controller. (WIP)

--- applications/demosSandbox/sdkDemos/demos/BasicRagdoll.cpp
+++ applications/demosSandbox/sdkDemos/demos/BasicRagdoll.cpp
@@ -69,9 +69,9 @@ static dPasiveRagDollDefinition skeletonRagDoll[] =
 	{"Bip01_L_Calf",     "capsule",   one_dof,   0.0f, 90.0f,  0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f,  10.0f,    -5.0f, 140.0f,     0.0f, 180.0f,  -90.0f,   0.0f}, 
 	{"Bip01_L_Foot",  "convexhull",   two_dof,   0.0f, 0.0f,   0.0f, 0.0f, 0.0f,  0.0f, 0.0f,  0.0f,   3.0f,      0.0f,  0.0f,     45.0f,	 0.0f,  -90.0f,  -0.0f}, 
 
-//	{ "Bip01_R_Thigh",   "capsule", two_dof, 0.0f, 90.0f, 0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f, 14.0f, -30.0f, 30.0f, -120.0f, 120.0f, -60.0f, 60.0f, 0.0f, -90.0f, -0.0f },
-//	{ "Bip01_R_Calf",    "capsule", one_dof, 0.0f, 90.0f, 0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f, 10.0f, -150.0f, 0.0f, 0.0f, 90.0f, 0.0f, -0.0f, 0.0f, 0.0f, 90.0f },
-//	{ "Bip01_R_Foot", "convexhull", two_dof, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 3.0f, 0.0f, 0.0f, -45.0f, 45.0f, -45.0f, 45.0f, 0.0f, -90.0f, -0.0f },
+	{"Bip01_R_Thigh",    "capsule", three_dof,   0.0f, 90.0f,  0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f,  14.0f,   -45.0f,  45.0f,   120.0f,   0.0f,  -90.0f,  -0.0f},
+	{"Bip01_R_Calf",     "capsule",   one_dof,   0.0f, 90.0f,  0.0f, 0.0f, 0.0f, 0.19f, 0.05f, 0.34f,  10.0f,    -5.0f, 140.0f,     0.0f, 180.0f,  -90.0f,   0.0f}, 
+	{"Bip01_R_Foot",  "convexhull",   two_dof,   0.0f, 0.0f,   0.0f, 0.0f, 0.0f,  0.0f, 0.0f,  0.0f,   3.0f,      0.0f,  0.0f,     45.0f,	 0.0f,  -90.0f,  -0.0f}, 
 
 
 //	{"Bip01_Spine",		 "capsule", 0.0f, 0.0f, -90.0f, 0.0f, 0.0f, 0.06f, 0.07f, 0.14f,  20.0f,    30.0f,  -30.0f,  30.0f,		0.0f, -90.0f, 0.0f,	   0.0f, -90.0f, 0.0f}, 
@@ -437,7 +437,7 @@ class PassiveRagdollManager: public dCustomArticulaledTransformManager
 		NewtonBody* const rootbody = controller->GetBoneBody(controller->GetBone(0));
 
 		MySaveLoad saveLoad(GetWorld());
-		saveLoad.Save (fileName, rootbody);
+		//saveLoad.Save (fileName, rootbody);
 	}
 
 	int m_material;
