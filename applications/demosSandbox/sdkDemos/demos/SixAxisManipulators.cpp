4691d4dba7b8a6117c97894ec7d23d3f4639766e
NewtonDynamics <jerezjulio0@gmail.com>
2017-11-30 16:13:03 -0800

    more work

--- applications/demosSandbox/sdkDemos/demos/SixAxisManipulators.cpp
+++ applications/demosSandbox/sdkDemos/demos/SixAxisManipulators.cpp
@@ -84,28 +84,28 @@ class dSixAxisController: public dCustomControllerBase
 
 		NewtonBody* const parent = CreateCylinder(scene, location, 0.35f, 0.125f);
 		NewtonBodySetMassMatrix(parent, 0.0f, 0.0f, 0.0f, 0.0f);
+//		void* const baseNode = NewtonInverseDynamicsAddRoot(m_kinematicSolver, parent);
 
 		dMatrix baseMatrix(dGetIdentityMatrix());
 		baseMatrix.m_posit = location.m_posit;
 		baseMatrix.m_posit.m_y += 0.125f * 0.5f + 0.11f;
 		baseMatrix.m_posit.m_z += 0.125f * 0.5f;
-
 		NewtonBody* const base = CreateBox(scene, baseMatrix, dVector(0.125f, 0.2f, 0.25f));
-
 		dMatrix baseSpin(dGrammSchmidt(dVector(0.0f, 1.0f, 0.0f)));
 		baseSpin.m_posit = location.m_posit;
 		dKukaServoMotor* const baseHinge = new dKukaServoMotor(baseSpin, base, parent);
+//		void* const baseHingeNode = NewtonInverseDynamicsAddChildNode(m_kinematicSolver, baseNode, baseHinge->GetJoint());
 
 		dMatrix arm0Matrix(dPitchMatrix(45.0f * 3.141592f / 180.0f));
 		arm0Matrix.m_posit = baseMatrix.m_posit;
 		arm0Matrix.m_posit.m_y += 0.30f;
 		arm0Matrix.m_posit.m_x += 0.09f;
 		arm0Matrix.m_posit.m_z -= 0.125f;
-
 		NewtonBody* const arm0 = CreateBox(scene, arm0Matrix, dVector(0.05f, 0.1f, 0.75f));
 		dMatrix arm0HingeMatrix(dGrammSchmidt(dVector(1.0f, 0.0f, 0.0f)));
 		arm0HingeMatrix.m_posit = arm0Matrix.m_posit + arm0Matrix.RotateVector(dVector(0.0f, 0.0f, 0.3f));
 		dKukaServoMotor* const arm0Hinge = new dKukaServoMotor(arm0HingeMatrix, arm0, base);
+//		void* const arm0HingeNode = NewtonInverseDynamicsAddChildNode(m_kinematicSolver, baseHingeNode, arm0Hinge->GetJoint());
 
 		dMatrix arm1Matrix(arm0Matrix * dYawMatrix(3.131592f));
 		arm1Matrix.m_posit = arm0Matrix.m_posit;
@@ -113,10 +113,12 @@ class dSixAxisController: public dCustomControllerBase
 		arm1Matrix.m_posit.m_x -= 0.05f;
 		arm1Matrix.m_posit.m_z -= 0.1f;
 		NewtonBody* const arm1 = CreateBox(scene, arm1Matrix, dVector(0.05f, 0.1f, 0.5f));
-
 		dMatrix arm1HingeMatrix(dGrammSchmidt(dVector(1.0f, 0.0f, 0.0f)));
 		arm1HingeMatrix.m_posit = arm1Matrix.m_posit + arm1Matrix.RotateVector(dVector(0.0f, 0.0f, 0.2f));
 		dKukaServoMotor* const arm1Hinge = new dKukaServoMotor(arm1HingeMatrix, arm1, arm0);
+//		void* const arm1HingeNode = NewtonInverseDynamicsAddChildNode(m_kinematicSolver, arm0HingeNode, arm1Hinge->GetJoint());
+
+//		NewtonInverseDynamicsEndBuild(m_kinematicSolver);
 	}
 
 	private:
