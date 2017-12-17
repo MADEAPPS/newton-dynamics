b991bde8d78903d0d7b48c6c6136aa6f05eb551b
JulioJerez <jerezjulio0@gmail.com>
2017-12-17 15:36:14 -0800

    disable gravity until setup is completed

--- applications/demosSandbox/sdkDemos/toolBox/PhysicsUtils.cpp
+++ applications/demosSandbox/sdkDemos/toolBox/PhysicsUtils.cpp
@@ -609,7 +609,7 @@ void  PhysicsApplyGravityForce (const NewtonBody* body, dFloat timestep, int thr
 	dVector dir(0.0f, 1.0f, 0.0f);
 
 	dVector force (dir.Scale (mass * DEMO_GRAVITY));
-	NewtonBodySetForce (body, &force.m_x);
+//	NewtonBodySetForce (body, &force.m_x);
 }
 
 
