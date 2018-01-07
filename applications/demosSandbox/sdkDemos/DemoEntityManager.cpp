6a1d044eb66253f0876baa1f46fc4871078e7efb
JulioJerez <jerezjulio0@gmail.com>
2018-01-07 15:11:18 -0800

    going back to the biped demo.

--- applications/demosSandbox/sdkDemos/DemoEntityManager.cpp
+++ applications/demosSandbox/sdkDemos/DemoEntityManager.cpp
@@ -63,8 +63,8 @@
 //#define DEFAULT_SCENE	29			// six axis manipulator
 //#define DEFAULT_SCENE	30			// hexapod Robot
 //#define DEFAULT_SCENE	31			// articulated joints
-#define DEFAULT_SCENE	32			// basic rag doll
-//#define DEFAULT_SCENE	33			// dynamics rag doll
+//#define DEFAULT_SCENE	32			// basic rag doll
+#define DEFAULT_SCENE	33			// dynamics rag doll
 //#define DEFAULT_SCENE	34			// basic Car
 //#define DEFAULT_SCENE	35			// super Car
 //#define DEFAULT_SCENE	36			// heavy vehicles
