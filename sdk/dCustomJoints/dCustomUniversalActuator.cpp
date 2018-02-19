347486540f24eee3144586eb06c1705d6f6799f3
NewtonDynamics <jerezjulio0@gmail.com>
2018-02-19 12:23:31 -0800

    work on universal actuator (WIP)

--- sdk/dCustomJoints/dCustomUniversalActuator.cpp
+++ sdk/dCustomJoints/dCustomUniversalActuator.cpp
@@ -124,10 +124,24 @@ void dCustomUniversalActuator::SetMaxTorque1(dFloat torque)
 
 void dCustomUniversalActuator::SubmitAngularRow(const dMatrix& matrix0, const dMatrix& matrix1, const dVector& eulers, dFloat timestep)
 {
-//	dCustomHingeActuator::SubmitAngularRow(matrix0, matrix1, eulers, timestep);
 	dCustomUniversal::SubmitAngularRow(matrix0, matrix1, eulers, timestep);
-/*
 
+	dFloat jointAngle0 = GetJointAngle();
+	dFloat targetAngle0 = m_targetAngle0.GetAngle();
+	dFloat relAngle0 = jointAngle0 - targetAngle0;
+	dFloat currentSpeed0 = GetJointOmega();
+	dFloat step0 = dFloat(2.0f) * m_motorSpeed * timestep;
+
+	dFloat desiredSpeed0 = (dAbs(relAngle0) > dAbs(step0)) ? -dSign(relAngle0) * m_motorSpeed : -dFloat(0.1f) * relAngle0 / timestep;
+	dFloat accel0 = (desiredSpeed0 - currentSpeed0) / timestep;
+
+	NewtonUserJointAddAngularRow(m_joint, 0.0f, &matrix0.m_front[0]);
+//	NewtonUserJointSetRowAcceleration(m_joint, accel0);
+	NewtonUserJointSetRowMinimumFriction(m_joint, -m_maxTorque0);
+	NewtonUserJointSetRowMaximumFriction(m_joint, m_maxTorque0);
+	NewtonUserJointSetRowStiffness(m_joint, m_stiffness);
+
+/*
 	if (m_actuator_0 | m_actuator_1){
 		dMatrix matrix0;
 		dMatrix matrix1;
