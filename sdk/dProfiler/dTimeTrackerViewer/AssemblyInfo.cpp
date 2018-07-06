4179c8e8067f48da0c184f6053b90ef577650355
NewtonDynamics <jerezjulio0@gmail.com>
2018-07-06 14:44:00 -0700

    work on profiler (WIP)

--- sdk/dProfiler/dTimeTrackerViewer/AssemblyInfo.cpp
+++ sdk/dProfiler/dTimeTrackerViewer/AssemblyInfo.cpp
@@ -37,4 +37,4 @@ using namespace System::Security::Permissions;
 
 [assembly:CLSCompliantAttribute(true)];
 
-[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
+//[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
--- sdk/dProfiler/dTimeTrackerViewer/Form1.h
+++ sdk/dProfiler/dTimeTrackerViewer/Form1.h
@@ -34,6 +34,9 @@ namespace dTimeTrackerViewer {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::ToolStrip^  toolStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripButton^  toolStripButton1;
 
 	private:
 		/// <summary>
@@ -48,19 +51,51 @@ namespace dTimeTrackerViewer {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->toolStrip1 = (gcnew System::Windows::Forms::ToolStrip());
+			this->toolStripButton1 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->toolStrip1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this->toolStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->toolStripButton1 });
+			this->toolStrip1->Location = System::Drawing::Point(0, 0);
+			this->toolStrip1->Name = L"toolStrip1";
+			this->toolStrip1->Size = System::Drawing::Size(1023, 25);
+			this->toolStrip1->TabIndex = 0;
+			this->toolStrip1->Text = L"toolStrip1";
+			// 
+			// toolStripButton1
+			// 
+			this->toolStripButton1->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->toolStripButton1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton1.Image")));
+			this->toolStripButton1->ImageTransparentColor = System::Drawing::Color::Magenta;
+			this->toolStripButton1->Name = L"toolStripButton1";
+			this->toolStripButton1->Size = System::Drawing::Size(23, 22);
+			this->toolStripButton1->Text = L"Load Capture";
+			this->toolStripButton1->Click += gcnew System::EventHandler(this, &Form1::toolStripButton1_Click);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1023, 540);
+			this->Controls->Add(this->toolStrip1);
 			this->Name = L"Form1";
-			this->Text = L"Form1";
+			this->Text = L"Time Tracker Viewer";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->toolStrip1->ResumeLayout(false);
+			this->toolStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void toolStripButton1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
 	};
 }
 
