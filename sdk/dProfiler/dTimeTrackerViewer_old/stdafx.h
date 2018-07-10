2b33af56ee1b53eeebdc4d128e878e8657ea7072
JulioJerez <jerezjulio0@gmail.com>
2018-07-10 05:33:31 -0700

    start migrating profiler viewer to imgui (WIP)

--- sdk/dProfiler/dTimeTrackerViewer_old/stdafx.h
+++ sdk/dProfiler/dTimeTrackerViewer_old/stdafx.h
@@ -1,19 +1,7 @@
 // stdafx.h : include file for standard system include files,
 // or project specific include files that are used frequently, but
 // are changed infrequently
-//
-
 #pragma once
 
-#include <stdio.h>
-#include <tchar.h>
-
 // TODO: reference additional headers your program requires here
-#include <imgui.h>
-#include <glfw3.h>
-#ifdef _WIN32
-#undef APIENTRY
-#define GLFW_EXPOSE_NATIVE_WIN32
-#define GLFW_EXPOSE_NATIVE_WGL
-#include <glfw3native.h>
-#endif
+#include <assert.h>
