1a761fa946e398b4e9e2439bc7ada1c4eded67be
JulioJerez <jerezjulio0@gmail.com>
2018-09-27 22:15:35 -0700

    display GPU name

--- sdk/dgNewtonDx12/dgWorldBase.cpp
+++ sdk/dgNewtonDx12/dgWorldBase.cpp
@@ -60,8 +60,8 @@ dgWorldBase::~dgWorldBase()
 
 const char* dgWorldBase::GetId() const
 {
-//	return m_deviceName;
-	return "gpu experimental";
+	return m_deviceName;
+//	return "gpu experimental";
 }
 
 dgInt32 dgWorldBase::GetScore() const
