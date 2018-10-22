47b8aa99d7bb32bd100bb61b7c6d16319f643619
JulioJerez <jerezjulio0@gmail.com>
2018-10-22 06:54:57 -0700

    forget this file

--- applications/demosSandbox/sdkDemos/DemoEntityManager.cpp
+++ applications/demosSandbox/sdkDemos/DemoEntityManager.cpp
@@ -662,7 +662,7 @@ void DemoEntityManager::ApplyMenuOptions()
 void DemoEntityManager::ShowMainMenuBar()
 {
 	int mainMenu = 0;
-dAssert (m_autoSleepMode);
+	dAssert (m_autoSleepMode);
 	if (ImGui::BeginMainMenuBar())
 	{
 		if (ImGui::BeginMenu("File")) {
