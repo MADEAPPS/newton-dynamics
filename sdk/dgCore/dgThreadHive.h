cdd530e4b430b38b963e5988a1d181f70f5f69c1
NewtonDynamics <jerezjulio0@gmail.com>
2018-12-11 10:43:01 -0800

    testing optimized thread pool

--- sdk/dgCore/dgThreadHive.h
+++ sdk/dgCore/dgThreadHive.h
@@ -30,7 +30,7 @@
 #define DG_THREAD_POOL_JOB_SIZE (256)
 typedef void (*dgWorkerThreadTaskCallback) (void* const context0, void* const context1, dgInt32 threadID);
 
-#define USE_OLD_THREAD_POOL 
+//#define USE_OLD_THREAD_POOL 
 
 #ifdef USE_OLD_THREAD_POOL 
 class dgThreadHive  
