c87be42c9332caba323a9d217a647cb960cfc68d
JulioJerez <jerezjulio0@gmail.com>
2019-01-06 18:27:36 -0800

    fixed casting in michio demo.

--- applications/demosSandbox/sdkDemos/demos/MishosRocketTest.cpp
+++ applications/demosSandbox/sdkDemos/demos/MishosRocketTest.cpp
@@ -362,8 +362,8 @@ void MishosHingeTest(DemoEntityManager* const scene)
 	NewtonCompoundCollisionBeginAddRemove(CoreCompound);
 	NewtonCompoundCollisionAddSubCollision(CoreCompound, CoreShape);
 	NewtonCompoundCollisionEndAddRemove(CoreCompound);
-	dMatrix matrixCore(dGetIdentityMatrix());
-	matrixCore.m_posit = dVector(dFloat(dVeryFar), dFloat(0.0f), dFloat(0.0f), dFloat(1.0f));
+	//  dMatrix matrixCore(0.0f, 0.0f, 1.5708f, dVector(0.0f, -30.0f, 0.0f));
+	dMatrix matrixCore(0.0f, 0.0f, 1.5708f, dVector(dVeryFar, dFloat (0.0f), dFloat (0.0f), dFloat (1.0f)));
 
 	DemoMesh* const geometryCore = new DemoMesh("Core Mesh", CoreCompound, "metal_30.tga", "metal_30.tga", "metal_30.tga");
 	NewtonBody* const CoreBody = CreateSimpleSolid(scene, geometryCore, CoreMass, matrixCore, CoreCompound, 0);
