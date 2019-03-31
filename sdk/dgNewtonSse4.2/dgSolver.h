ba913b48850acdb4f744de8385cf5fb92a590f59
JulioJerez <jerezjulio0@gmail.com>
2019-03-31 11:33:01 -0700

    fixed syntact error with scalar vectors class.

--- sdk/dgNewtonSse4.2/dgSolver.h
+++ sdk/dgNewtonSse4.2/dgSolver.h
@@ -35,7 +35,8 @@
 	class dgSoaFloat
 	{
 		public:
-		#define PERMUTE_MASK(w, z, y, x)		_MM_SHUFFLE (w, z, y, x)
+		//#define PERMUTE_MASK(w, z, y, x)		_MM_SHUFFLE (w, z, y, x)
+		#define PERMUT_MASK_DOUBLE(y, x)	_MM_SHUFFLE2 (y, x)
 
 		DG_INLINE dgSoaFloat()
 		{
@@ -48,8 +49,10 @@
 		}
 
 		DG_INLINE dgSoaFloat(const dgVector& type)
-			:m_low(type.m_typeLow)
-			,m_high(type.m_typeHigh)
+//			:m_low(type.m_typeLow)
+//			,m_high(type.m_typeHigh)
+			:m_low(_mm_set_pd(type.m_y, type.m_x))
+			,m_high(_mm_set_pd(type.m_w, type.m_z))
 		{
 		}
 
