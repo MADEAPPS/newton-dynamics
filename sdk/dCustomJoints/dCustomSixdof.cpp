5dbd2ec4c082a0eefe199024041211ced3d7fe40
Julio Jerez <jerezjulio0@gmail.com>
2019-07-01 12:31:52 -0700

    need to correct the six dof joint linear rows so that does not detach so much (WIP)

--- sdk/dCustomJoints/dCustomSixdof.cpp
+++ sdk/dCustomJoints/dCustomSixdof.cpp
@@ -311,7 +311,8 @@ void dCustomSixdof::SubmitConstraints (dFloat timestep, int threadIndex)
 			if ((m_minLinearLimits[i] == 0.0f) && (m_maxLinearLimits[i] == 0.0f)) {
 				const dVector& dir = matrix1[i];
 				dVector prejectPoint(p0 - dir.Scale(dir.DotProduct3(dp)));
-				NewtonUserJointAddLinearRow(m_joint, &p0[0], &prejectPoint[0], &dir[0]);
+				//NewtonUserJointAddLinearRow(m_joint, &p0[0], &prejectPoint[0], &dir[0]);
+				NewtonUserJointAddLinearRow(m_joint, &p0[0], &p1[0], &dir[0]);
 				NewtonUserJointSetRowStiffness(m_joint, m_stiffness);
 			} else {
 				dFloat posit = dp.DotProduct3(matrix1[i]);
