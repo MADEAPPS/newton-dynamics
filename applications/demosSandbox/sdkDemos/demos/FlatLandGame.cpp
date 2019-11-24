80fb1f02e056dc36d68a6b2181217fd3c20b192e
JulioJerez <jerezjulio0@gmail.com>
2019-11-24 12:06:25 -0800

    adding a 2d upvector joint for Lax game

--- applications/demosSandbox/sdkDemos/demos/FlatLandGame.cpp
+++ applications/demosSandbox/sdkDemos/demos/FlatLandGame.cpp
@@ -16,6 +16,60 @@
 #include "DemoMesh.h"
 #include "toolBox/OpenGlUtil.h"
 
+class Plane2dUpVector: public dCustomPlane
+{
+	public:
+	Plane2dUpVector (const dVector& pivot, const dVector& normal, NewtonBody* const child)
+		:dCustomPlane (pivot, normal, child)
+	{
+	}
+
+	void SubmitConstraints (dFloat timestep, int threadIndex)
+	{
+		// add one row to prevent body from rotating on the plane of motion
+		dMatrix matrix0;
+		dMatrix matrix1;
+		CalculateGlobalMatrix(matrix0, matrix1);
+
+		dFloat pitchAngle = CalculateAngle(matrix0.m_up, matrix1.m_up, matrix1.m_front);
+		NewtonUserJointAddAngularRow(m_joint, -pitchAngle, &matrix1.m_front[0]);
+
+		// add rows to fix matrix body the the plane
+		dCustomPlane::SubmitConstraints(timestep, threadIndex);
+	}
+};
+
+static void AddPlayerBodies (DemoEntityManager* const scene, NewtonBody* const floor)
+{
+
+	NewtonWorld* const world = scene->GetNewton();
+	dVector size(1.0f, 1.0f, 1.0f, 0.0f);
+
+	dMatrix location(dGetIdentityMatrix());
+
+	location.m_posit.m_y = 4.0f;
+
+	dMatrix gitzmoMatrix (dRollMatrix(90.0f * dDegreeToRad));
+	NewtonCollision* const collision = CreateConvexCollision(world, dGetIdentityMatrix(), size, _CAPSULE_PRIMITIVE, 0);
+	NewtonCollisionSetMatrix(collision, &gitzmoMatrix[0][0]);
+
+	DemoMesh* const geometry = new DemoMesh("table", scene->GetShaderCache(), collision, "frowny.tga", "logo_php.tga", "smilli.tga");
+
+	location.m_posit.m_z = 10.0f;
+	for (int i = 0; i < 4; i++) {
+		NewtonBody* const box = CreateSimpleSolid(scene, geometry, 10.0f, location, collision, 0);
+
+		// constrain these object to motion on the plane only
+		dMatrix matrix;
+		NewtonBodyGetMatrix(box, &matrix[0][0]);
+		new Plane2dUpVector(matrix.m_posit, matrix.m_front, box);
+		location.m_posit.m_z += 2.5f;
+	}
+
+	geometry->Release();
+	NewtonDestroyCollision(collision);
+}
+
 static void AddGravityBodies (DemoEntityManager* const scene, NewtonBody* const floor)
 {
 	NewtonWorld* const world = scene->GetNewton();
@@ -70,13 +124,14 @@ void FlatLandGame (DemoEntityManager* const scene)
 {
 	scene->CreateSkyBox();
 
-//	NewtonWorld* const world = scene->GetNewton();
-
 	// make a floor for a 2d world
 	NewtonBody* const ground = CreateBackground (scene);
 
 	// add some object constrained to a move on the x plane
 	AddGravityBodies (scene, ground);
+
+	// add players 
+	AddPlayerBodies (scene, ground);
 
 	// place camera into position
 	dMatrix camMatrix (dGetIdentityMatrix());
