0771f4f11746c34e2ea98abc2c686886709b41f2
NewtonDynamics <jerezjulio0@gmail.com>
2020-02-07 14:42:46 -0800

    simplifying repro bug to make eassier to debug. (WIP)

--- applications/demosSandbox/sdkDemos/demos/BasicStacking.cpp
+++ applications/demosSandbox/sdkDemos/demos/BasicStacking.cpp
@@ -132,7 +132,6 @@ static void BuildJenga (DemoEntityManager* const scene, dFloat mass, const dVect
 	NewtonDestroyCollision (collision);
 }
 
-
 static void BuildPyramid (DemoEntityManager* const scene, dFloat mass, const dVector& origin, const dVector& size, int count, PrimitiveType type, const dMatrix& shapeMatrix = dGetIdentityMatrix())
 {
 	dMatrix matrix (dGetIdentityMatrix());
@@ -214,10 +213,44 @@ static void SphereStack(DemoEntityManager* const scene, dFloat mass, const dVect
 	// create the shape and visual mesh as a common data to be re used
 	NewtonCollision* const collision = CreateConvexCollision(world, dGetIdentityMatrix(), blockBoxSize, _SPHERE_PRIMITIVE, defaultMaterialID);
 	DemoMesh* const geometry = new DemoMesh("sphere", scene->GetShaderCache(), collision, "wood_0.tga", "wood_0.tga", "wood_0.tga");
+#if 0
 	for (int i = 0; i < count; i++) {
 		CreateSimpleSolid(scene, geometry, mass, baseMatrix, collision, defaultMaterialID);
 		baseMatrix.m_posit += baseMatrix.m_up.Scale(blockBoxSize.m_x);
 	}
+#else
+	int material = NewtonMaterialGetDefaultGroupID(world);
+	NewtonMaterialSetDefaultElasticity(world, material, material, 0.0f);
+
+	dArray<NewtonBody*> array;
+	baseMatrix.m_posit.m_y += 1.0f;
+	for (int i = 0; i < count; i++) {
+		array[i] = CreateSimpleSolid(scene, geometry, mass, baseMatrix, collision, defaultMaterialID);
+		//baseMatrix = dYawMatrix(20.0f * dDegreeToRad) * baseMatrix;
+		baseMatrix.m_posit += baseMatrix.m_up.Scale(blockBoxSize.m_x);
+	}
+
+	for (int i = 1; i < count - 1; i++) {
+		dMatrix matrix0;
+		dMatrix matrix1;
+		NewtonBodyGetMatrix(array[i], &matrix0[0][0]);
+		NewtonBodyGetMatrix(array[i + 1], &matrix1[0][0]);
+		matrix0.m_posit = (matrix0.m_posit + matrix1.m_posit).Scale(0.5f);
+		dCustomBallAndSocket* const joint = new  dCustomBallAndSocket(matrix0, array[i], array[i + 1]);
+		joint->EnableTwist(true);
+		joint->EnableCone(true);
+		joint->SetConeLimits(0.0f);
+		joint->SetTwistLimits(0.0f, 0.0f);
+	}
+
+	dFloat Ixx;
+	dFloat Iyy;
+	dFloat Izz;
+	dFloat mass1;
+	dFloat scale = 100.0f;
+	NewtonBodyGetMass(array[count - 1], &mass1, &Ixx, &Iyy, &Izz);
+	NewtonBodySetMassMatrix(array[count - 1], mass1 * scale, Ixx * scale, Iyy * scale, Izz * scale);
+#endif
 
 	//baseMatrix.m_posit += baseMatrix.m_up.Scale(blockBoxSize.m_x * 4.0f);
 	DemoMesh* const geometry1 = new DemoMesh("sphere", scene->GetShaderCache(), collision, "wood_1.tga", "wood_1.tga", "wood_1.tga");
@@ -428,9 +461,9 @@ void BasicBoxStacks (DemoEntityManager* const scene)
 high = 3;
 	for (int i = 0; i < 1; i ++) {
 		for (int j = 0; j < 1; j ++) {
-//			SphereStack(scene, 1.0f, dVector(-5.0f + j * 8, 0.0f, -6.0f + i * 8, 0.0f), dVector (0.5f, 0.5f, 0.5f, 0.0f), high);
+			SphereStack(scene, 1.0f, dVector(-5.0f + j * 8, 0.0f, -6.0f + i * 8, 0.0f), dVector (0.5f, 0.5f, 0.5f, 0.0f), high);
 //			CapsuleStack (scene, 1.0f, dVector(-5.0f + j * 8, 0.0f, -14.0f + i * 8, 0.0f), dVector (0.8f, 4.0f, 0.8f, 0.0f), high);
-			BoxStack(scene, 1.0f, dVector(-5.5f + j * 8, 0.0f, 6.0f + i * 8, 0.0f), dVector (0.5f, 0.5f, 0.5f, 0.0f), high);
+//			BoxStack(scene, 1.0f, dVector(-5.5f + j * 8, 0.0f, 6.0f + i * 8, 0.0f), dVector (0.5f, 0.5f, 0.5f, 0.0f), high);
 		}
 	}
 
