682c565fdc8ce8c71a625d94a89c0e07b2b81bad
NewtonDynamics <jerezjulio0@gmail.com>
2020-02-25 11:22:09 -0800

    debugging picking asymatic bodies
    some big bug

--- applications/demosSandbox/sdkDemos/DemoEntityManager.cpp
+++ applications/demosSandbox/sdkDemos/DemoEntityManager.cpp
@@ -36,7 +36,7 @@
 //#define DEFAULT_SCENE	1		// coefficients of friction
 //#define DEFAULT_SCENE	2		// coefficients of restitution
 //#define DEFAULT_SCENE	3		// newton cradle
-//#define DEFAULT_SCENE	4		// gyroscope precession
+#define DEFAULT_SCENE	4		// gyroscope precession
 //#define DEFAULT_SCENE	5		// closest distance
 //#define DEFAULT_SCENE	6		// primitive collision
 //#define DEFAULT_SCENE	7		// kinematic bodies
@@ -70,7 +70,7 @@
 //#define DEFAULT_SCENE	35		// heavy vehicles
 //#define DEFAULT_SCENE	36		// basic player controller
 //#define DEFAULT_SCENE	37		// animated player controller
-#define DEFAULT_SCENE	38		// advanced player controller
+//#define DEFAULT_SCENE	38		// advanced player controller
 //#define DEFAULT_SCENE	39		// cloth patch			
 //#define DEFAULT_SCENE	40		// soft bodies	
 						 
