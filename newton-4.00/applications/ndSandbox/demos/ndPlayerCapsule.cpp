9537bbb229195991671dcfe8ce9a658ea9eff1f7
JulioJerez <jerezjulio0@gmail.com>
2020-10-17 09:16:49 -0700

    start adding obstacles to collide with.

--- newton-4.00/applications/ndSandbox/demos/ndPlayerCapsule.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndPlayerCapsule.cpp
@@ -162,12 +162,75 @@ static void BuildFloor(ndDemoEntityManager* const scene)
 	geometry->Release();
 }
 
+
+static void AddShape(ndDemoEntityManager* const scene,
+	ndDemoMesh* const sphereMesh, const ndShapeInstance& sphereShape,
+	dFloat32 mass, const dVector& origin, const dFloat32 diameter, int count, dFloat32 xxxx)
+{
+	//dMatrix matrix(dGetIdentityMatrix());
+	dMatrix matrix(dRollMatrix(90.0f * dDegreeToRad));
+	//dMatrix matrix(dYawMatrix(90.0f * dDegreeToRad) * dPitchMatrix(-45.0f * dDegreeToRad));
+	matrix.m_posit = origin;
+	matrix.m_posit.m_w = 1.0f;
+
+	ndPhysicsWorld* const world = scene->GetWorld();
+
+	dVector floor(FindFloor(*world, matrix.m_posit + dVector(0.0f, 100.0f, 0.0f, 0.0f), 200.0f));
+	matrix.m_posit.m_y = floor.m_y + diameter * 0.5f * 0.99f;
+
+	matrix.m_posit.m_y += 10.0f;
+
+	for (dInt32 i = 0; i < count; i++)
+	{
+		ndBodyDynamic* const body = new ndBodyDynamic();
+		ndDemoEntity* const entity = new ndDemoEntity(matrix, nullptr);
+		entity->SetMesh(sphereMesh, dGetIdentityMatrix());
+
+		body->SetNotifyCallback(new ndDemoEntityNotify(scene, entity));
+		body->SetMatrix(matrix);
+		body->SetCollisionShape(sphereShape);
+		body->SetMassMatrix(mass, sphereShape);
+		body->SetGyroMode(true);
+
+		world->AddBody(body);
+		scene->AddEntity(entity);
+
+		//matrix.m_posit.m_y += diameter * 0.99f;
+		matrix.m_posit.m_y += diameter * 0.99f * 3.0f;
+	}
+}
+
+static void AddShapes(ndDemoEntityManager* const scene, const dVector& origin)
+{
+	dFloat32 diameter = 1.0f;
+	//ndShapeInstance shape(new ndShapeSphere(diameter * 0.5f));
+	ndShapeInstance shape(new ndShapeCapsule(diameter * 0.5f, diameter * 0.5f, diameter * 1.0f));
+	//ndShapeInstance shape(new ndShapeBox(diameter, diameter, diameter));
+	//ndDemoMesh* const mesh = new ndDemoMesh("shape", scene->GetShaderCache(), &shape, "wood_0.tga", "wood_0.tga", "wood_0.tga");
+	ndDemoMesh* const mesh = new ndDemoMesh("shape", scene->GetShaderCache(), &shape, "marble.tga", "marble.tga", "marble.tga");
+
+	const int n = 2;
+	const int stackHigh = 2;
+	//const int n = 10;
+	//const int stackHigh = 7;
+	for (dInt32 i = 0; i < n; i++)
+	{
+		for (dInt32 j = 0; j < n; j++)
+		{
+			dVector location((j - n / 2) * 4.0f, 0.0f, (i - n / 2) * 4.0f, 0.0f);
+			AddShape(scene, mesh, shape, 10.0f, location + origin, 1.0f, stackHigh, 2.0f);
+		}
+	}
+
+	mesh->Release();
+}
+
+
 void ndPlayerCapsuleDemo (ndDemoEntityManager* const scene)
 {
 	// build a floor
 	BuildFloor(scene);
 
-	dVector origin1(0.0f, 0.0f, 0.0f, 0.0f);
 	dMatrix location(dGetIdentityMatrix());
 	location.m_posit.m_y += 2.0f;
 
@@ -183,6 +246,9 @@ void ndPlayerCapsuleDemo (ndDemoEntityManager* const scene)
 		scene, localAxis, location, mass, radio, height, height/3.0f);
 
 	scene->SetUpdateCameraFunction(ndBasicPlayer::UpdateCameraCallback, player);
+
+	dVector origin1(8.0f, 0.0f, 0.0f, 0.0f);
+	AddShapes(scene, origin1);
 
 	dQuaternion rot;
 	dVector origin(-10.0f, 5.0f, 0.0f, 0.0f);
