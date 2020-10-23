0ae4949936e177fee24ce1cf36de4182415825f3
JulioJerez <jerezjulio0@gmail.com>
2020-10-23 10:26:41 -0700

    start loading mesh collision map.

---
+++ newton-4.00/applications/ndSandbox/toolBox/ndMakeStaticMap.cpp
@@ -0,0 +1,20 @@
+/* Copyright (c) <2003-2019> <Newton Game Dynamics>
+* 
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* 
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely
+*/
+
+#include "ndSandboxStdafx.h"
+#include "ndTargaToOpenGl.h"
+#include "ndDemoMesh.h"
+#include "ndDemoCamera.h"
+#include "ndPhysicsUtils.h"
+#include "ndPhysicsWorld.h"
+#include "ndMakeStaticMap.h"
+#include "ndDemoEntityManager.h"
+
---
+++ newton-4.00/applications/ndSandbox/toolBox/ndMakeStaticMap.h
@@ -0,0 +1,23 @@
+/* Copyright (c) <2003-2019> <Newton Game Dynamics>
+* 
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* 
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely
+*/
+
+#ifndef _D_MAKE_STATIC_MAP_H_
+#define _D_MAKE_STATIC_MAP_H_
+
+#include "ndSandboxStdafx.h"
+
+class ndDemoMesh;
+class ndDemoEntity;
+class ndDemoEntityManager;
+
+
+
+#endif
