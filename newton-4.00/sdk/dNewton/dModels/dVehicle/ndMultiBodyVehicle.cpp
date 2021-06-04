f7ad03be08d2e08084ba60f55ec859bc745f08d1
JulioJerez <jerezjulio0@gmail.com>
2021-06-04 06:33:21 -0700

    made tire restitution 0.1 instead of 0.4

--- newton-4.00/sdk/dNewton/dModels/dVehicle/ndMultiBodyVehicle.cpp
+++ newton-4.00/sdk/dNewton/dModels/dVehicle/ndMultiBodyVehicle.cpp
@@ -469,11 +469,11 @@ void ndMultiBodyVehicle::BrushTireModel(const ndJointWheel* const tire, ndContac
 	const dFloat32 longitudinalFrictionCoefficient = frictionCoefficient * cx / gamma;
 	//dTrace(("%f %f\n", sideSpeed, lateralFrictionCoefficient));
 
+	contactPoint.m_material.m_restitution = dFloat32 (0.1f);
 	contactPoint.m_material.m_staticFriction0 = lateralFrictionCoefficient;
 	contactPoint.m_material.m_dynamicFriction0 = lateralFrictionCoefficient;
 	contactPoint.m_material.m_staticFriction1 = longitudinalFrictionCoefficient;
 	contactPoint.m_material.m_dynamicFriction1 = longitudinalFrictionCoefficient;
-	//contactPoint.m_material.m_restitution = 0.0f;
 }
 
 void ndMultiBodyVehicle::ApplyTiremodel()
