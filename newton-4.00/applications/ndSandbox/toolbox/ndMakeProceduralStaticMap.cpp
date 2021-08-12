48bffd3da37df3973e6ddfa0832557dce5a80ff2
JulioJerez <jerezjulio0@gmail.com>
2021-08-12 08:24:03 -0700

    start adding a procedural static mesh

--- newton-4.00/applications/ndSandbox/toolbox/ndMakeProceduralStaticMap.cpp
+++ newton-4.00/applications/ndSandbox/toolbox/ndMakeProceduralStaticMap.cpp
@@ -18,6 +18,98 @@
 #include "ndDemoEntityManager.h"
 #include "ndMakeProceduralStaticMap.h"
 
+ndDemoEntity* BuildVisualEntiry(ndDemoEntityManager* const scene, dInt32 grids, dFloat32 gridSize, dFloat32 perturbation)
+{
+	dVector origin(-grids * gridSize * 0.5f, 0.0f, -grids * gridSize * 0.5f, 0.0f);
+
+	dArray<dVector> points;
+	for (dInt32 iz = 0; iz <= grids; iz++)
+	{
+		dFloat32 z0 = origin.m_z + iz * gridSize;
+		for (dInt32 ix = 0; ix <= grids; ix++)
+		{
+			dFloat32 x0 = origin.m_x + ix * gridSize;
+			points.PushBack(dVector(x0, dGaussianRandom(perturbation), z0, 1.0f));
+		}
+	}
+
+	ndMeshEffect meshEffect;
+	meshEffect.BeginBuild();
+	for (dInt32 iz = 0; iz < grids; iz++)
+	{
+		for (dInt32 ix = 0; ix < grids; ix++)
+		{
+			dVector p0(points[(ix + 0) * (grids + 1) + iz + 0]);
+			dVector p1(points[(ix + 1) * (grids + 1) + iz + 0]);
+			dVector p2(points[(ix + 1) * (grids + 1) + iz + 1]);
+			dVector p3(points[(ix + 0) * (grids + 1) + iz + 1]);
+
+			meshEffect.BeginBuildFace();
+			meshEffect.AddPoint(p0.m_x, p0.m_y, p0.m_z);
+			meshEffect.AddPoint(p1.m_x, p1.m_y, p1.m_z);
+			meshEffect.AddPoint(p2.m_x, p2.m_y, p2.m_z);
+			meshEffect.EndBuildFace();
+
+			meshEffect.BeginBuildFace();
+			meshEffect.AddPoint(p0.m_x, p0.m_y, p0.m_z);
+			meshEffect.AddPoint(p2.m_x, p2.m_y, p2.m_z);
+			meshEffect.AddPoint(p3.m_x, p3.m_y, p3.m_z);
+			meshEffect.EndBuildFace();
+		}
+	}
+	meshEffect.EndBuild(0.0f);
+
+	dPolygonSoupBuilder meshBuilder;
+
+	meshBuilder.Begin();
+	dInt32 vertexStride = meshEffect.GetVertexStrideInByte() / sizeof(dFloat64);
+	const dFloat64* const vertexData = meshEffect.GetVertexPool();
+
+	dInt32 mark = meshEffect.IncLRU();
+
+	dVector face[16];
+	dPolyhedra::Iterator iter(meshEffect);
+	for (iter.Begin(); iter; iter++)
+	{
+		dEdge* const edge = &(*iter);
+		if ((edge->m_incidentFace >= 0) && (edge->m_mark != mark))
+		{
+			dInt32 count = 0;
+			dEdge* ptr = edge;
+			do
+			{
+				dInt32 i = ptr->m_incidentVertex * vertexStride;
+				dVector point(dFloat32(vertexData[i + 0]), dFloat32(vertexData[i + 1]), dFloat32(vertexData[i + 2]), dFloat32(1.0f));
+				face[count] = point;
+				count++;
+				ptr->m_mark = mark;
+				ptr = ptr->m_next;
+			} while (ptr != edge);
+
+			dInt32 materialIndex = meshEffect.GetFaceMaterial(edge);
+			meshBuilder.AddFace(&face[0].m_x, sizeof(dVector), 3, materialIndex);
+		}
+	}
+	meshBuilder.End(false);
+
+	dFloat32 uvScale = 1.0 / 16.0f;
+
+	ndShapeInstance plane(new ndShapeStatic_bvh(meshBuilder));
+	dMatrix uvMatrix(dGetIdentityMatrix());
+	uvMatrix[0][0] *= uvScale;
+	uvMatrix[1][1] *= uvScale;
+	uvMatrix[2][2] *= uvScale;
+	ndDemoMesh* const geometry = new ndDemoMesh("plane", scene->GetShaderCache(), &plane, "marbleCheckBoard.tga", "marbleCheckBoard.tga", "marbleCheckBoard.tga", 1.0f, uvMatrix);
+
+	dMatrix matrix(dGetIdentityMatrix());
+	ndDemoEntity* const entity = new ndDemoEntity(matrix, nullptr);
+	entity->SetMesh(geometry, dGetIdentityMatrix());
+
+	scene->AddEntity(entity);
+	geometry->Release();
+	return entity;
+}
+
 ndBodyKinematic* BuildProceduralMap(ndDemoEntityManager* const scene, dInt32 grids, dFloat32 gridSize, dFloat32 perturbation)
 {
 	dVector origin(-grids * gridSize * 0.5f, 0.0f, -grids * gridSize * 0.5f, 0.0f);
@@ -93,28 +185,14 @@ ndBodyKinematic* BuildProceduralMap(ndDemoEntityManager* const scene, dInt32 gri
 	}
 	meshBuilder.End(false);
 
-	dFloat32 uvScale = 1.0 / 16.0f;
-
-	ndShapeInstance plane(new ndShapeStatic_bvh(meshBuilder));
-	dMatrix uvMatrix(dGetIdentityMatrix());
-	uvMatrix[0][0] *= uvScale;
-	uvMatrix[1][1] *= uvScale;
-	uvMatrix[2][2] *= uvScale;
-	ndDemoMesh* const geometry = new ndDemoMesh("plane", scene->GetShaderCache(), &plane, "marbleCheckBoard.tga", "marbleCheckBoard.tga", "marbleCheckBoard.tga", 1.0f, uvMatrix);
-
 	dMatrix matrix(dGetIdentityMatrix());
-	ndDemoEntity* const entity = new ndDemoEntity(matrix, nullptr);
-	entity->SetMesh(geometry, dGetIdentityMatrix());
+	ndShapeInstance plane(new ndShapeStatic_bvh(meshBuilder));
 
 	ndBodyDynamic* const body = new ndBodyDynamic();
-	body->SetNotifyCallback(new ndDemoEntityNotify(scene, entity));
 	body->SetMatrix(matrix);
 	body->SetCollisionShape(plane);
-
+	body->SetNotifyCallback(new ndDemoEntityNotify(scene, BuildVisualEntiry(scene, grids, gridSize, perturbation)));
 	world->AddBody(body);
-
-	scene->AddEntity(entity);
-	geometry->Release();
 	return body;
 }
 
