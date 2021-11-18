0a43958e447db9a4c13d3c8371317ac7a4e417eb
NewtonDynamics <jerezjulio0@gmail.com>
2021-11-18 10:43:18 -0800

    debug pervious bug. (wip)

--- newton-4.00/sdk/dNewton/ndDynamicsUpdate.cpp
+++ newton-4.00/sdk/dNewton/ndDynamicsUpdate.cpp
@@ -2148,6 +2148,7 @@ void ndDynamicsUpdate::CalculateJointsForce()
 		virtual void Execute()
 		{
 			D_TRACKTIME();
+			dAssert(0);
 			const dVector zero(dVector::m_zero);
 			ndWorld* const world = m_owner->GetWorld();
 			ndDynamicsUpdate* const me = (ndDynamicsUpdate*)world->m_solver;
