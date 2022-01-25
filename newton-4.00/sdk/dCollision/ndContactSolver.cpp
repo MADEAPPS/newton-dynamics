68bfc66666ab92db207d5f247dcf07abeb0e3235
JulioJerez <jerezjulio0@gmail.com>
2022-01-25 08:36:04 -0800

    added better debug code to fix a missing colliding pair.

--- newton-4.00/sdk/dCollision/ndContactSolver.cpp
+++ newton-4.00/sdk/dCollision/ndContactSolver.cpp
@@ -2464,7 +2464,7 @@ ndInt32 ndContactSolver::CalculateContactsDiscrete()
 	}
 	else
 	{
-		//dTrace(("!!!!!Fix compound contact\n"));
+		dTrace(("Fix compound contact for pair: %s %s\n", m_instance0.GetShape()->SubClassName(), m_instance1.GetShape()->SubClassName()));
 		dAssert(0);
 	}
 
