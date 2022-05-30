3686bb761c628cbfd6478a67797cd1a0d53ea890
NewtonDynamics <jerezjulio0@gmail.com>
2022-05-30 12:19:36 -0700

    stress testing cuda work so far. (now we have a robust working version)

--- newton-4.00/applications/ndSandbox/demos/ndBasicGpuRigidBody.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndBasicGpuRigidBody.cpp
@@ -131,7 +131,7 @@ void ndBasicGpuRigidBody(ndDemoEntityManager* const scene)
 
 	//ndInt32 count = 50;
 	//ndInt32 count = 40;
-	//ndInt32 count = 32;
+	ndInt32 count = 32;
 	//ndInt32 count = 28;
 	//ndInt32 count = 20;
 	//ndInt32 count = 15;
@@ -140,7 +140,7 @@ void ndBasicGpuRigidBody(ndDemoEntityManager* const scene)
 	//ndInt32 count = 8;
 	//ndInt32 count = 7;
 	//ndInt32 count = 6;
-	ndInt32 count = 5;
+	//ndInt32 count = 5;
 	//ndInt32 count = 4;
 	//ndInt32 count = 3;
 	//ndInt32 count = 2;
