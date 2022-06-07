0acd26c344a9dc0ecc42b69ceeeb04640f7018e3
NewtonDynamics <jerezjulio0@gmail.com>
2022-06-07 12:08:43 -0700

    some how tracy does not connect
    need to build the server

--- newton-4.00/sdk/dProfiler/dTracyProfiler.cpp
+++ newton-4.00/sdk/dProfiler/dTracyProfiler.cpp
@@ -32,23 +32,10 @@
 	{
 		if (profileOn) 
 		{
-			//const auto thread = GetThreadHandle();
-			//Magic magic;
-			//auto token = GetToken();
-			//auto& tail = token->get_tail_index();
-			//auto item = token->enqueue_begin<tracy::moodycamel::CanAlloc>(magic);
-			//MemWrite(&item->hdr.type, QueueType::ZoneBegin);
-			//#ifdef TRACY_RDTSCP_OPT
-			//			MemWrite(&item->zoneBegin.time, Profiler::GetTime(item->zoneBegin.cpu));
-			//#else
-			//			uint32_t cpu;
-			//			MemWrite(&item->zoneBegin.time, Profiler::GetTime(cpu));
-			//			MemWrite(&item->zoneBegin.cpu, cpu);
-			//#endif
-			//MemWrite(&item->zoneBegin.thread, thread);
-			//MemWrite(&item->zoneBegin.srcloc, (uint64_t)srcloc);
-			//tail.store(magic + 1, std::memory_order_release);
-			//return thread;
+			TracyQueuePrepare(QueueType::ZoneBegin);
+			MemWrite(&item->zoneBegin.time, Profiler::GetTime());
+			MemWrite(&item->zoneBegin.srcloc, (uint64_t)srcloc);
+			TracyQueueCommit(zoneBeginThread);
 			return 0;
 		} 
 		else 
@@ -57,25 +44,14 @@
 		}
 	}
 
-	void dProfilerEndTraceLow(long long threadId)
+	//void dProfilerEndTraceLow(long long threadId)
+	void dProfilerEndTraceLow(long long)
 	{
 		if (profileOn) 
 		{
-			//Magic magic;
-			//std::thread::native_handle_type thread = (std::thread::native_handle_type) threadId;
-			//auto token = GetToken();
-			//auto& tail = token->get_tail_index();
-			//auto item = token->enqueue_begin<tracy::moodycamel::CanAlloc>(magic);
-			//MemWrite(&item->hdr.type, QueueType::ZoneEnd);
-			//#ifdef TRACY_RDTSCP_OPT
-			//			MemWrite(&item->zoneEnd.time, Profiler::GetTime(item->zoneEnd.cpu));
-			//#else
-			//			uint32_t cpu;
-			//			MemWrite(&item->zoneEnd.time, Profiler::GetTime(cpu));
-			//			MemWrite(&item->zoneEnd.cpu, cpu);
-			//#endif
-			//MemWrite(&item->zoneEnd.thread, thread);
-			//tail.store(magic + 1, std::memory_order_release);
+			TracyQueuePrepare(QueueType::ZoneEnd);
+			MemWrite(&item->zoneEnd.time, Profiler::GetTime());
+			TracyQueueCommit(zoneEndThread);
 		}
 	}
 
@@ -83,6 +59,7 @@
 	{
 		//std::thread::native_handle_type handle = (std::thread::native_handle_type) GetThreadHandle();
 		//SetThreadName(handle, trackName);
+		tracy::SetThreadName(trackName);
 	}
 
 #else
