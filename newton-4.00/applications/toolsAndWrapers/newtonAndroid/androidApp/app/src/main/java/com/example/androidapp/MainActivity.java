e86c874e551c362d85b5d3e176410a843e1dc2fa
JulioJerez <jerezjulio0@gmail.com>
2022-09-27 04:16:44 -0700

    adding the goggle open gl es 3 sample "2011 The Android Open Source Project"  for simple visual demo (wip)

--- newton-4.00/applications/toolsAndWrapers/newtonAndroid/androidApp/app/src/main/java/com/example/androidapp/MainActivity.java
+++ newton-4.00/applications/toolsAndWrapers/newtonAndroid/androidApp/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.opengl.GLSurfaceView;
 
 import com.javaNewton.nWorld;
 import com.javaNewton.nMatrix;
@@ -22,7 +24,8 @@ public class MainActivity extends AppCompatActivity
         System.loadLibrary("ndNewton");
     }
 
-   nWorld world;
+   private nWorld world;
+   private GLSurfaceView mGLView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,7 +38,48 @@ public class MainActivity extends AppCompatActivity
 		world.SetSubSteps(2);
 		
 		TestEngine();
+
+		// Create a GLSurfaceView instance and set it
+		// as the ContentView for this Activity
+		mGLView = new MyGLSurfaceView(this);
+		setContentView(mGLView);
     }
+
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		// The following call pauses the rendering thread.
+		// If your OpenGL application is memory intensive,
+		// you should consider de-allocating objects that
+		// consume significant memory here.
+		mGLView.onPause();
+    }
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		// The following call resumes a paused rendering thread.
+		// If you de-allocated graphic objects for onPause()
+		// this is a good place to re-allocate them.
+		mGLView.onResume();
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus)
+	{
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus) {
+			getWindow().getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+					View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+					View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+					View.SYSTEM_UI_FLAG_HIDE_NAVIGATION	|
+					View.SYSTEM_UI_FLAG_FULLSCREEN);
+		}
+	}
+
 
 	protected void AddFloor()
 	{
