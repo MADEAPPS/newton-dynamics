681b982ad6909b0ed37f28007342409c46eb3d34
Julio Jerez <julio.jerez.-nd@disneystreaming.com>
2023-04-18 16:37:54 -0700

    start loading all standard joint (wip)

--- newton-4.00/applications/ndSandbox/ndDemoEntityManager.cpp
+++ newton-4.00/applications/ndSandbox/ndDemoEntityManager.cpp
@@ -47,14 +47,14 @@
 //#define DEFAULT_SCENE	7		// particle fluid
 //#define DEFAULT_SCENE	8		// static mesh collision 
 //#define DEFAULT_SCENE	9		// static user mesh collision 
-//#define DEFAULT_SCENE	10		// basic joints
+#define DEFAULT_SCENE	10		// basic joints
 //#define DEFAULT_SCENE	11		// basic vehicle
 //#define DEFAULT_SCENE	12		// heavy vehicle
 //#define DEFAULT_SCENE	13		// background vehicle prop
 //#define DEFAULT_SCENE	14		// simple industrial robot
 //#define DEFAULT_SCENE	15		// advanced industrial robot
 //#define DEFAULT_SCENE	16		// basic player
-#define DEFAULT_SCENE	17		// rag doll
+//#define DEFAULT_SCENE	17		// rag doll
 //#define DEFAULT_SCENE	18		// zero moment point
 //#define DEFAULT_SCENE	19		// quadruped test 1
 //#define DEFAULT_SCENE	20		// quadruped test 2
