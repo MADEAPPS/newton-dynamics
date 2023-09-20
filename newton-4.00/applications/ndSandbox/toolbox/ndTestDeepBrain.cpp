f8006069d74ae44b458a21102b648f2d071dc311
NewtonDynamics <jerezjulio0@gmail.com>
2023-09-20 06:30:55 -0700

    last mnist twist

--- newton-4.00/applications/ndSandbox/toolbox/ndTestDeepBrain.cpp
+++ newton-4.00/applications/ndSandbox/toolbox/ndTestDeepBrain.cpp
@@ -333,10 +333,16 @@ static void MnistTrainingSet()
 
 			// so far best training result on the mnist data set
 			//optimizer.SetRegularizer(ndReal(2.0e-5f));
-			optimizer.SetRegularizer(ndReal(2.5e-5f));
+			//optimizer.SetRegularizer(ndReal(2.5e-5f));
+			//optimizer.SetRegularizer(ndReal(3.0e-5f));
+			//optimizer.SetRegularizer(ndReal(3.5e-5f));
+			//optimizer.SetRegularizer(ndReal(4.0e-5f));
+			optimizer.SetRegularizer(ndReal(4.5e-5f)); //best score yet 98.35% on test data
+			//optimizer.SetRegularizer(ndReal(4.6e-5f));
+			//optimizer.SetRegularizer(ndReal(5.0e-5f));
 
 			bool firstPass = true;
-			for (ndInt32 i = 0; i < 2000000; ++i)
+			for (ndInt32 i = 0; i < 10000000; ++i)
 			{
 				ndInt32 priorityStart = ndMin(priorityIndexArray.GetCount(), 2);
 				for (ndInt32 j = 0; j < priorityStart; ++j)
