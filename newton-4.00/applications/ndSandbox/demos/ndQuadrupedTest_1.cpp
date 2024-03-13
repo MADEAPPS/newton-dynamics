3de80bfc98052f7f9025b073dd86b2b572a73e22
NewtonDynamics <jerezjulio0@gmail.com>
2024-03-13 07:09:02 -0700

    setting  a 100 million step training session

--- newton-4.00/applications/ndSandbox/demos/ndQuadrupedTest_1.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndQuadrupedTest_1.cpp
@@ -284,7 +284,7 @@ namespace ndQuadruped_1
 				,m_timer(ndGetTimeInMicroseconds())
 				,m_maxGain(-1.0e10f)
 				,m_maxFrames(3500)
-				,m_stopTraining(50000000)
+				,m_stopTraining(100 * 1000000)
 				,m_lastEpisode(-1)
 				,m_modelIsTrained(false)
 			{
