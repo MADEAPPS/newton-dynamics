a773664903489dfc470766be6aa3d3edb54204d1
Julio Jerez <julio.jerez.-nd@disneystreaming.com>
2024-05-21 10:34:58 -0700

    start making quad 1 walk experiment (wip)

--- newton-4.00/applications/ndSandbox/ndDemoEntityManager.cpp
+++ newton-4.00/applications/ndSandbox/ndDemoEntityManager.cpp
@@ -47,7 +47,7 @@
 //#define DEFAULT_SCENE	7		// particle fluid
 //#define DEFAULT_SCENE	8		// static mesh collision 
 //#define DEFAULT_SCENE	9		// static user mesh collision 
-#define DEFAULT_SCENE	10		// basic joints
+//#define DEFAULT_SCENE	10		// basic joints
 //#define DEFAULT_SCENE	11		// basic vehicle
 //#define DEFAULT_SCENE	12		// heavy vehicle
 //#define DEFAULT_SCENE	13		// background vehicle prop
@@ -58,7 +58,7 @@
 //#define DEFAULT_SCENE	18		// cart pole discrete controller
 //#define DEFAULT_SCENE	19		// cart pole continue controller
 //#define DEFAULT_SCENE	20		// unit cycle controller
-//#define DEFAULT_SCENE	21		// quadruped test 1
+#define DEFAULT_SCENE	21		// quadruped test 1
 //#define DEFAULT_SCENE	22		// quadruped test 2
 //#define DEFAULT_SCENE	23		// quadruped test 3
 //#define DEFAULT_SCENE	24		// biped test 1
