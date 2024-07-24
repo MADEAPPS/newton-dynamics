7c85538421ac88104328fff2d827476c94d11708
JulioJerez <jerezjulio0@gmail.com>
2024-07-24 07:10:26 -0700

    experiments, trying lower learn rate.

--- newton-4.00/sdk/dBrain/ndBrainAgentContinuePolicyGradient_Trainer.h
+++ newton-4.00/sdk/dBrain/ndBrainAgentContinuePolicyGradient_Trainer.h
@@ -166,10 +166,10 @@ class ndBrainAgentContinuePolicyGradient_TrainerMaster : public ndBrainThreadPoo
 			//m_baseLineOptimizationPases = 4;
 			m_baseLineOptimizationPases = 1;
 
-			m_criticLearnRate = ndBrainFloat(0.0005f);
-			m_policyLearnRate = ndBrainFloat(0.0002f);
-			//m_criticLearnRate = ndBrainFloat(0.0002f);
-			//m_policyLearnRate = ndBrainFloat(0.0001f);
+			//m_criticLearnRate = ndBrainFloat(0.0005f);
+			//m_policyLearnRate = ndBrainFloat(0.0002f);
+			m_criticLearnRate = ndBrainFloat(0.0002f);
+			m_policyLearnRate = ndBrainFloat(0.0001f);
 
 			m_regularizer = ndBrainFloat(1.0e-6f);
 			m_discountFactor = ndBrainFloat(0.99f);
