38ba1f9079023d5f3b972e63d05a1e90310c29b1
NewtonDynamics <jerezjulio0@gmail.com>
2024-08-16 13:18:28 -0700

    trying apply hello world app.

---
+++ newton-4.00/applications/ndAppleSandBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Julio Jerez on 8/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newton-4.00/applications/ndAppleSandBox/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Julio Jerez on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
