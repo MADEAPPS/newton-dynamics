8fdc8c53e777eb43b5970b1454ad1a85d0fd5670
NewtonDynamics <jerezjulio0@gmail.com>
2024-08-18 10:06:14 -0700

    more set up (wip)

--- newton-4.00/apple/demos/demo_01/CppInteropInlineExample/SimpleCxxFunctions.cpp
+++ newton-4.00/apple/demos/demo_01/CppInteropInlineExample/SimpleCxxFunctions.cpp
@@ -5,7 +5,7 @@
 //  Created by Labtanza on 8/9/23.
 //
 
-#include "SimpleCxxFunctions.hpp"
+#include "SimpleCxxFunctions.h"
 //#include "SimpleSwiftFunctions-Swift.h" why is this not made?
 
 int myFavoriteNumber() {
