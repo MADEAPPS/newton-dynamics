a304bbfad6c828dd362e195e27f0a86ea9079d2e
JulioJerez <jerezjulio0@gmail.com>
2024-08-19 08:48:47 -0700

    fixing mac compiling warnings

--- newton-4.00/thirdParty/openFBX/src/miniz.c
+++ newton-4.00/thirdParty/openFBX/src/miniz.c
@@ -50,17 +50,17 @@ mz_ulong mz_adler32(mz_ulong adler, const unsigned char *ptr, size_t buf_len)
     {
         for (i = 0; i + 7 < block_len; i += 8, ptr += 8)
         {
-            s1 += ptr[0], s2 += s1;
-            s1 += ptr[1], s2 += s1;
-            s1 += ptr[2], s2 += s1;
-            s1 += ptr[3], s2 += s1;
-            s1 += ptr[4], s2 += s1;
-            s1 += ptr[5], s2 += s1;
-            s1 += ptr[6], s2 += s1;
-            s1 += ptr[7], s2 += s1;
+            s1 += ptr[0]; s2 += s1;
+            s1 += ptr[1]; s2 += s1;
+            s1 += ptr[2]; s2 += s1;
+            s1 += ptr[3]; s2 += s1;
+            s1 += ptr[4]; s2 += s1;
+            s1 += ptr[5]; s2 += s1;
+            s1 += ptr[6]; s2 += s1;
+            s1 += ptr[7]; s2 += s1;
         }
         for (; i < block_len; ++i)
-            s1 += *ptr++, s2 += s1;
+            s1 += *ptr++; s2 += s1;
         s1 %= 65521U, s2 %= 65521U;
         buf_len -= block_len;
         block_len = 5552;
