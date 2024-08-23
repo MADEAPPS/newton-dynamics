a556c0fc94687198f87183b71b6fa683f2ea2c34
JulioJerez <jerezjulio0@gmail.com>
2024-08-23 07:52:30 -0700

    set training step to 200 millions. and start training

--- newton-4.00/applications/ndSandbox/demos/ndQuadrupedTest_2.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndQuadrupedTest_2.cpp
@@ -1269,7 +1269,7 @@ namespace ndQuadruped_2
 			,m_discountFactor(0.995f)
 			,m_horizon(ndFloat32(1.0f) / (ndFloat32(1.0f) - m_discountFactor))
 			,m_lastEpisode(-1)
-			,m_stopTraining(500 * 1000000)
+			,m_stopTraining(200 * 1000000)
 			,m_modelIsTrained(false)
 		{
 			ndWorld* const world = scene->GetWorld();
