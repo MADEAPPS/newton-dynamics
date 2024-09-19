dcfe189ffc04e28517dde8a34b0d7b5a3a02fff6
JulioJerez <jerezjulio0@gmail.com>
2024-09-19 01:45:02 -0700

    using larger default learn rate (20 time larger)

--- newton-4.00/sdk/dBrain/ndBrainAgentContinuePolicyGradient_Trainer.cpp
+++ newton-4.00/sdk/dBrain/ndBrainAgentContinuePolicyGradient_Trainer.cpp
@@ -48,10 +48,12 @@ ndBrainAgentContinuePolicyGradient_TrainerMaster::HyperParameters::HyperParamete
 	m_numberOfActions = 0;
 	m_numberOfObservations = 0;
 
-	m_criticLearnRate = ndBrainFloat(0.0004f);
-	m_policyLearnRate = ndBrainFloat(0.0002f);
+	//m_criticLearnRate = ndBrainFloat(0.0004f);
+	//m_policyLearnRate = ndBrainFloat(0.0002f);
 	//m_criticLearnRate = ndBrainFloat(0.0002f);
 	//m_policyLearnRate = ndBrainFloat(0.0001f);
+	m_criticLearnRate = ndBrainFloat(0.0005f);
+	m_policyLearnRate = ndBrainFloat(0.001f);
 
 	m_regularizer = ndBrainFloat(1.0e-6f);
 	m_discountFactor = ndBrainFloat(0.99f);
