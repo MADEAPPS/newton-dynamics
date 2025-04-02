b48237669ace1e8b213da7194405cd2824e3b8f1
NewtonDynamics <jerezjulio0@gmail.com>
2025-04-02 09:39:24 -0700

    using gradient of log prob instead of grad of prob

--- newton-4.00/sdk/dBrain/ndBrainAgentContinuePolicyGradient_Trainer.cpp
+++ newton-4.00/sdk/dBrain/ndBrainAgentContinuePolicyGradient_Trainer.cpp
@@ -756,6 +756,7 @@ ndFloat32 ndBrainAgentContinuePolicyGradient_TrainerMaster::GetAverageScore() co
 	return m_averageScore.GetAverage();
 }
 
+#pragma optimize( "", off )
 void ndBrainAgentContinuePolicyGradient_TrainerMaster::OptimizePolicy()
 {
 	ndAtomic<ndInt32> iterator(0);
@@ -785,6 +786,7 @@ void ndBrainAgentContinuePolicyGradient_TrainerMaster::OptimizePolicy()
 				{
 				}
 
+#if 0
 				void GetLoss(const ndBrainVector& probabilityDistribution, ndBrainVector& loss)
 				{
 					// basically this fits a multivariate Gaussian process with zero cross covariance to the actions.
@@ -808,6 +810,30 @@ void ndBrainAgentContinuePolicyGradient_TrainerMaster::OptimizePolicy()
 						loss[i + numberOfActions] = -sigmaGradient * advantage;
 					}
 				}
+#else
+
+				void GetLoss(const ndBrainVector& probabilityDistribution, ndBrainVector& loss)
+				{
+					// basically this fits a multivariate Gaussian process with zero cross covariance to the actions.
+					const ndInt32 numberOfActions = m_agent->m_numberOfActions;
+					const ndBrainFloat advantage = m_agent->m_trajectoryAccumulator.GetAdvantage(m_index);
+					const ndBrainMemVector sampledProbability(m_agent->m_trajectoryAccumulator.GetActions(m_index), numberOfActions * 2);
+
+					for (ndInt32 i = numberOfActions - 1; i >= 0; --i)
+					{
+						const ndBrainFloat mean = probabilityDistribution[i];
+						const ndBrainFloat sigma1 = probabilityDistribution[i + numberOfActions];
+						const ndBrainFloat sigma2 = sigma1 * sigma1;
+						const ndBrainFloat num = sampledProbability[i] - mean;
+
+						ndBrainFloat meanGradient = num / sigma1;
+						ndBrainFloat sigmaGradient = ndBrainFloat(0.5f) * (num * num / sigma2 - ndBrainFloat(1.0f) / sigma1);
+
+						loss[i] = meanGradient * advantage;
+						loss[i + numberOfActions] = sigmaGradient * advantage;
+					}
+				}
+#endif
 
 				ndBrainTrainer& m_trainer;
 				ndBrainAgentContinuePolicyGradient_TrainerMaster* m_agent;
