16070e3fb78ddf4af4694229f507d53a723872eb
NewtonDynamics <jerezjulio0@gmail.com>
2025-04-29 12:04:15 -0700

    Update ndBrainAgentDeterministicPolicyGradient_Trainer.h

--- newton-4.00/sdk/dBrain/ndBrainAgentDeterministicPolicyGradient_Trainer.h
+++ newton-4.00/sdk/dBrain/ndBrainAgentDeterministicPolicyGradient_Trainer.h
@@ -208,7 +208,7 @@ class ndBrainAgentDeterministicPolicyGradient_Trainer : public ndBrainThreadPool
 	ndBrainAgentDeterministicPolicyGradient_Agent* m_agent;
 	ndArray<ndInt32> m_shuffleBuffer;
 	ndMovingAverage<8> m_averageExpectedRewards;
-	ndMovingAverage<32> m_averageFramesPerEpisodes;
+	ndMovingAverage<8> m_averageFramesPerEpisodes;
 	ndUnsigned32 m_frameCount;
 	ndUnsigned32 m_framesAlive;
 	ndUnsigned32 m_eposideCount;
