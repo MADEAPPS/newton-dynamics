01baaf26093ae90db9597e72632b274b7a8cacef
NewtonDynamics <jerezjulio0@gmail.com>
2025-06-30 08:06:17 -0700

    start adding gpu emulation for debug puporse

---
+++ newton-4.00/sdk/dBrain/cpuEmulation/ndBrainGpuBuffer.cpp
@@ -0,0 +1,53 @@
+/* Copyright (c) <2003-2022> <Newton Game Dynamics>
+* 
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* 
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely
+*/
+
+#include "ndBrainStdafx.h"
+#include "ndBrainBuffer.h"
+#include "ndBrainGpuBuffer.h"
+
+#if 0
+#include "ndBrainVector.h"
+
+#include "ndBrainGpuContext.h"
+
+ndBrainGpuBuffer::ndBrainGpuBuffer(ndBrainGpuContext* const context, ndInt64 sizeInBytes, ndStorageBufferType)
+	:m_context (context)
+	,m_sizeInBytes(size_t(sizeInBytes))
+{
+}
+
+ndBrainGpuBuffer::~ndBrainGpuBuffer()
+{
+}
+
+
+size_t ndBrainGpuBuffer::SizeInBytes() const
+{ 
+	return m_sizeInBytes; 
+}
+
+ndBrainGpuFloatBuffer* ndBrainGpuBuffer::GetAsFloatBuffer()
+{
+	return nullptr;
+}
+
+ndBrainGpuUniformBuffer* ndBrainGpuBuffer::GetAsUniformBuffer()
+{
+	return nullptr;
+}
+
+#endif
+
+ndBrainGpuBuffer::ndBrainGpuBuffer(const ndBrainBuffer* const owner, bool)
+	:m_owner(owner)
+{
+	m_memory.SetCount(ndInt64(m_owner->SizeInBytes()));
+}
---
+++ newton-4.00/sdk/dBrain/cpuEmulation/ndBrainGpuBuffer.h
@@ -0,0 +1,29 @@
+/* Copyright (c) <2003-2022> <Newton Game Dynamics>
+* 
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* 
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely
+*/
+#ifndef __ND_BRAIN_GPU_BUFFER_H__
+#define __ND_BRAIN_GPU_BUFFER_H__
+
+#include "ndBrainStdafx.h"
+
+class ndBrainBuffer;
+class ndBrainContext;
+
+class ndBrainGpuBuffer : public ndClassAlloc
+{
+	ndBrainGpuBuffer(const ndBrainBuffer* const owner, bool memoryMapped);
+
+	const ndBrainBuffer* m_owner;
+	ndArray<ndUnsigned8> m_memory;
+
+	friend class ndBrainBuffer;
+};
+
+#endif
