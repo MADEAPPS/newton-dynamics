fd8e6d698612265cd7c7953aaae2e45d503431fd
JulioJerez <jerezjulio0@gmail.com>
2025-08-21 03:29:26 -0700

    Update ndQuadruped_sac_test.cpp

--- newton-4.00/applications/ndSandbox/demos/ndQuadruped_sac_test.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndQuadruped_sac_test.cpp
@@ -28,7 +28,7 @@
 // to the environment with increasing complexity
 namespace ndQuadruped_sac
 {
-	#define ND_TRAIN_MODEL
+	//#define ND_TRAIN_MODEL
 	#define CONTROLLER_NAME "ndQuadruped_2-sac.dnn"
 
 	enum Actions
@@ -866,7 +866,7 @@ namespace ndQuadruped_sac
 			context.DrawPoint(zmp, ndVector(1.0f, 0.0f, 0.0f, 1.0f), 4);
 
 			//// draw zero moment point surrogate point, define as: 
-			//// a point propertinal to the center of mass angular acceleration
+			//// a point proportional to the center of mass angular acceleration
 			//// projected onto the support polygon plane.
 			//ndFloat32 xAlpha = dynamics.m_alpha.m_z / gravityForce;
 			//ndFloat32 zAlpha = -dynamics.m_alpha.m_x / gravityForce;
@@ -955,7 +955,7 @@ namespace ndQuadruped_sac
 		ndModelArticulation::ndNode* const modelRootNode = model->AddRootBody(rootBody);
 		for (ndList<ndSharedPtr<ndDemoEntity>>::ndNode* node = entity->GetChildren().GetFirst(); node; node = node->GetNext())
 		{
-			// build thig
+			// build thigh
 			ndSharedPtr<ndDemoEntity> thighEntity(node->GetInfo());
 			const ndMatrix thighMatrix(thighEntity->GetCurrentMatrix() * matrix);
 			ndSharedPtr<ndBody> thigh(CreateRigidBody(thighEntity, thighMatrix, limbMass, rootBody->GetAsBodyDynamic()));
