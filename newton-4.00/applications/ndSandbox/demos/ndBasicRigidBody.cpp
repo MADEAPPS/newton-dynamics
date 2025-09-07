f84849f95fbf13a5cc3fcc3ae7c797e699a8aa31
NewtonDynamics <jerezjulio0@gmail.com>
2025-09-07 08:38:46 -0700

    Update ndBasicRigidBody.cpp

--- newton-4.00/applications/ndSandbox/demos/ndBasicRigidBody.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndBasicRigidBody.cpp
@@ -21,7 +21,7 @@ void ndBasicRigidBody(ndDemoEntityManager* const scene)
 	//BuildFloorBox(scene, ndGetIdentityMatrix());
 	//ndSharedPtr<ndBody> body(BuildFlatPlane(scene, true, true));
 	//ndSharedPtr<ndBody> bodyFloor(BuildFloorBox(scene, ndGetIdentityMatrix(), "marbleCheckBoard.png", 0.1f, true));
-	//ndSharedPtr<ndBody> bodyFloor(BuildFloorBox(scene, ndGetIdentityMatrix(), "blueCheckerboard.png", 0.1f, true));
+	ndSharedPtr<ndBody> bodyFloor(BuildFloorBox(scene, ndGetIdentityMatrix(), "blueCheckerboard.png", 0.1f, true));
 
 	ndMatrix matrix(ndGetIdentityMatrix());
 
@@ -29,9 +29,9 @@ void ndBasicRigidBody(ndDemoEntityManager* const scene)
 	origin1.m_posit.m_x = 20.0f;
 	origin1.m_posit.m_y = 5.0f;
 
-	ndSharedPtr<ndBody> body(AddSphere(scene, origin1, 1.0f, 10.0f));
-	body->SetOmega(ndVector(0.0f, 2.0f, 0.0f, 0.0f));
-	body->SetMatrix(origin1);
+	//ndSharedPtr<ndBody> body(AddSphere(scene, origin1, 1.0f, 10.0f));
+	//body->SetOmega(ndVector(0.0f, 2.0f, 0.0f, 0.0f));
+	//body->SetMatrix(origin1);
 
 	//AddPlanks(scene, origin1, 1.0f, 4);
 	//AddCapsulesStacks(scene, origin1, 10.0f, 0.5f, 0.5f, 1.0f, 1, 2, 7);
