a18b5692ef000015204c75d180c4d59ce90e777d
NewtonDynamics <jerezjulio0@gmail.com>
2025-09-21 15:12:18 -0700

    Update ndBasicGyroTorque.cpp
    the rattleback is not working

--- newton-4.00/applications/ndSandbox/demos/ndBasicGyroTorque.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndBasicGyroTorque.cpp
@@ -154,7 +154,9 @@ static ndSharedPtr<ndBody> PrecessingTop(ndDemoEntityManager* const scene, const
 
 	ndRenderPrimitiveMesh::ndDescriptor descriptor(render);
 	descriptor.m_collision = shape;
-	descriptor.m_mapping = ndRenderPrimitiveMesh::m_cylindrical;
+	descriptor.m_uvMatrix = shape->GetLocalMatrix();
+	descriptor.m_mapping = ndRenderPrimitiveMesh::m_box;
+	descriptor.m_stretchMaping = false;
 	descriptor.AddMaterial(render->GetTextureCache()->GetTexture(ndGetWorkingFileName("smilli.png")));
 	ndSharedPtr<ndRenderPrimitive> mesh(ndRenderPrimitiveMesh::CreateMeshPrimitive(descriptor));
 
