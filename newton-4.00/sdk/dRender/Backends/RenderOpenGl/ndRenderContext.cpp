8914beb2c46e33804301957974fb7e9aa1f1114f
NewtonDynamics <jerezjulio0@gmail.com>
2025-09-21 19:50:54 -0700

    Update ndRenderContext.cpp
    remap all the ascii key to the imgui binding

--- newton-4.00/sdk/dRender/Backends/RenderOpenGl/ndRenderContext.cpp
+++ newton-4.00/sdk/dRender/Backends/RenderOpenGl/ndRenderContext.cpp
@@ -197,25 +197,28 @@ void ndRenderContext::InitImGui(const char* const fontPathName)
 	io.UserData = this;
 
 	// Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
-	//io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
-	//io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
-	//io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
-	//io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
-	//io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
-	//io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
-	//io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
-	//io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
-	//io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
-	//io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
-	//io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
-	//io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
-	//io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
-	//io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
-	//io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
-	//io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
-	//io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
-	//io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
-	//io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
+	io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
+	io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
+	io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
+	io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
+	io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
+	io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
+	io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
+	io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
+	io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
+	io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
+	io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
+	io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
+	io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
+	for (int i = 0; i < (GLFW_KEY_Z - GLFW_KEY_A + 1); ++i)
+	{
+		io.KeyMap[ImGuiKey_A + i] = GLFW_KEY_A + i;
+	}
+	//io.KeyMap[ImGuiKey_D] = GLFW_KEY_D;
+	//io.KeyMap[ImGuiKey_W] = GLFW_KEY_W;
+	//io.KeyMap[ImGuiKey_S] = GLFW_KEY_S;
+	//io.KeyMap[ImGuiKey_Q] = GLFW_KEY_Q;
+	//io.KeyMap[ImGuiKey_E] = GLFW_KEY_E;
 
 #ifdef _MSC_VER 
 	io.ImeWindowHandle = glfwGetWin32Window(m_mainFrame);
