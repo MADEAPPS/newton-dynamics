d648e5c505e4a9a50de7815da47cf43573677ad9
NewtonDynamics <jerezjulio0@gmail.com>
2025-10-03 10:34:16 -0700

    Revert "start adding roller-coaster demo (wip)"
    This reverts commit 1f1a00b72315ba5c16975e28da71429ba2cd4319.

--- newton-4.00/sdk/dRender/Backends/RenderOpenGl/ndRenderShaderCache.h
+++ newton-4.00/sdk/dRender/Backends/RenderOpenGl/ndRenderShaderCache.h
@@ -32,7 +32,6 @@ class ndRenderShaderCache
 		{
 			GLuint m_skyBoxEffect;
 			GLuint m_diffuseEffect;
-			GLuint m_wireFrameEffect;
 			GLuint m_setZbufferEffect;
 			GLuint m_diffuseShadowEffect;
 			GLuint m_diffuseShadowSkinEffect;
@@ -48,7 +47,6 @@ class ndRenderShaderCache
 
 	// vertex shaders
 	static const char* m_skyBoxVertex;
-	static const char* m_wireFrameVertex;
 	static const char* m_setZbufferVertex;
 	static const char* m_generateShadowMapVertex;
 	static const char* m_directionalDiffuseVertex;
@@ -62,7 +60,6 @@ class ndRenderShaderCache
 	// pixel shaders
 	static const char* m_skyBoxPixel;
 	static const char* m_doNothingPixel;
-	static const char* m_wireFramePixel;
 	static const char* m_directionalDiffusePixel;
 	static const char* m_debugFlatShadedDiffusePixel;
 	static const char* m_directionalDiffuseShadowPixel;
