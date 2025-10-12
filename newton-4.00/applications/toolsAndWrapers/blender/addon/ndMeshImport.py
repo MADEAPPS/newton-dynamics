84620b4ea5bec5ee565ecd2aa8dc51ca0e59a3f1
NewtonDynamics <jerezjulio0@gmail.com>
2025-10-12 12:49:49 -0700

    Update ndMeshImport.py
    more blende addon work (wip)

--- newton-4.00/applications/toolsAndWrapers/blender/addon/ndMeshImport.py
+++ newton-4.00/applications/toolsAndWrapers/blender/addon/ndMeshImport.py
@@ -1,4 +1,7 @@
 import bpy
+import math
+import bmesh
+import mathutils
 import xml.etree.ElementTree as ET
 
 def LoadMesh(context, filepath, use_some_setting):
@@ -17,28 +20,55 @@ def TestShit(xmlNode):
     angles = [float(x) for x in matrix.find('angles').get('float3').split()]
     print(posit, angles)
     
-def ParseGeomentry(node, xmlNode):
-    xmlVertices = xmlNode.find('vertices')
-    print(xmlVertices)
+def CalculateTransform(xmlMatrix):
+    posit = [float(x) for x in xmlMatrix.find('posit').get('float3').split()]
+    angles = [float(x) for x in xmlMatrix.find('angles').get('float3').split()]
+
+    scale = mathutils.Vector((1.0, 1.0, 1.0))
+    location = mathutils.Vector((posit[0], posit[1], posit[2]))
+    rotation = mathutils.Euler((math.radians(angles[0]), math.radians(angles[1]), math.radians(angles[2])), 'XYZ')
+    return mathutils.Matrix.LocRotScale(location, rotation, scale)
+
+def ParseVertices(meshObj, xmlVertices):
+    #positions = xmlVertices.find('vertices')
+    #indices = xmlVertices.find('indices')
+    indices = [int(x) for x in xmlVertices.find('indices').get('intArray').split()]
+    posit = [float(x) for x in xmlVertices.find('positions').get('float3Array').split()]
+    print(posit)
+    print(indices)
+
+
+def ParseGeomentry(nodeData, xmlNode):
+    mesh = bmesh.new()
+    ParseVertices(mesh, xmlNode.find('vertices'))
     
     
+    mesh.to_mesh(nodeData)
+    nodeData.update()
+    mesh.free()
+   
+    
+        
 def ParseNode(context, xmlNode, blenderParentNode):
     #TestShit(meshNode)
     
     #create a geometry and a mesh node and link it to the scene and parent
-    meshData = bpy.data.meshes.new(xmlNode.find('name').get('string'))
-    meshNode = bpy.data.objects.new(xmlNode.find('name').get('string'), meshData);
-
-    meshNode.parent_type = 'OBJECT'
-    meshNode.parent = blenderParentNode
-    bpy.context.scene.collection.objects.link(meshNode)
+    data = bpy.data.meshes.new(xmlNode.find('name').get('string'))
+    node = bpy.data.objects.new(xmlNode.find('name').get('string'), data);
+    
+    node.parent_type = 'OBJECT'
+    node.parent = blenderParentNode
+    bpy.context.scene.collection.objects.link(node)
+    
+    #set the transform
+    node.matrix_basis = CalculateTransform(xmlNode.find('matrix'))
     
     xmlGeometry = xmlNode.find('ndGeometry')
     if (xmlGeometry != None):
-        ParseGeomentry(meshNode, xmlGeometry)
+        ParseGeomentry(data, xmlGeometry)
     
     # add all the children nodes
     for book in xmlNode.findall('ndMesh'):
-        ParseNode(context, book, meshNode)
+        ParseNode(context, book, node)
         
-    return meshNode
+    return node
