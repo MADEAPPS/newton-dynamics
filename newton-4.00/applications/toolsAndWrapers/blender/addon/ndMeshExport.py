d803d01f41dbd24b17b6de9b329dd71555d93dfd
NewtonDynamics <jerezjulio0@gmail.com>
2025-10-28 10:08:38 -0700

    Update ndMeshExport.py
    start work on exporter

--- newton-4.00/applications/toolsAndWrapers/blender/addon/ndMeshExport.py
+++ newton-4.00/applications/toolsAndWrapers/blender/addon/ndMeshExport.py
@@ -14,12 +14,71 @@ import bmesh
 import mathutils
 import xml.etree.ElementTree as ET
 
+#
+# first convert the blend mesh to a ndMesh so that it can be post processed
+#
+class ndMesh:
+    def __init__ (self, context, blenderMesh):
+        self.name = "unnamed"
+        objectData = blenderMesh.data
+        if (objectData != None):
+            self.name = objectData.name
+
+        self.matrix = blenderMesh.matrix_basis
+        self.nodeTypes = ['node', 'bone', 'endbone']
+        self.type = self.nodeTypes[0]
+        
+        # save some blender node data info 
+        self.blederArmature = None
+        self.blenderNode = blenderMesh
+        for modifier in self.blenderNode.modifiers:
+            if modifier.type == 'ARMATURE':
+                self.blederArmature = modifier
+                break
+
+        self.children = []
+        for child in blenderMesh.children:
+           self.children.append(ndMesh(context, child))
+           
+    def MapArmature(self, rootNode):
+        print(self.name)
+           
+    def MapArmatures(self, rootNode):
+        if (self.blederArmature != None):
+            self.MapArmature(rootNode)
+
+        for child in self.children:
+            child.MapArmatures(rootNode)
+
+           
+    def SaveXml(self, xmlNode):
+        # save matrix
+        xmlName = ET.SubElement(xmlNode, "name")
+        xmlName.set('string', self.name)
+
+        # save matrix
+        xmlMatrix = ET.SubElement(xmlNode, "matrix")
+        location, quaternion, scale = self.matrix.decompose()
+    
+        eulers = quaternion.to_euler('XYZ')
+        degrees = mathutils.Vector((math.degrees(eulers.x), math.degrees(eulers.y), math.degrees(eulers.z)))
+        # ignore scale since is suppose to be 1.0
+        xmlPosit = ET.SubElement(xmlMatrix, "posit")
+        xmlPosit.set('float3', ' '.join(map(str, location)))
+        xmlAngles = ET.SubElement(xmlMatrix, "angles")
+        xmlAngles.set('float3', ' '.join(map(str, degrees)))
+
+        for child in self.children:
+            xmlChild = ET.SubElement(xmlNode, "ndMesh")
+            child.SaveXml(xmlChild)
+
+
 def SaveMesh(context, filepath):
-    scale = mathutils.Vector((1.0, 1.0, 1.0))
-    location = mathutils.Vector((0.0, 0.0, 0.0))
-    eulers = mathutils.Euler((math.radians(90.0), math.radians(0.0), math.radians(-90.0)), 'XYZ')
-    matrix = mathutils.Matrix.LocRotScale(location, eulers, scale)
-    matrix.invert()
+    #scale = mathutils.Vector((1.0, 1.0, 1.0))
+    #location = mathutils.Vector((0.0, 0.0, 0.0))
+    #eulers = mathutils.Euler((math.radians(90.0), math.radians(0.0), math.radians(-90.0)), 'XYZ')
+    #matrix = mathutils.Matrix.LocRotScale(location, eulers, scale)
+    #matrix.invert()
     
     # Exit edit mode before exporting, so current object states are exported properly.
     if bpy.ops.object.mode_set.poll():
@@ -27,18 +86,25 @@ def SaveMesh(context, filepath):
 
     #parse blender selected node and all it children
     object = context.object
+    
+    if object.type != 'MESH':
+        print("please select a Mesh Type of node")
+        return {'FINISHED'}
+    
+    
     xmlRoot = ET.Element("ndMesh")
-    SaveNodeDataRecurse(context, xmlRoot, object, matrix)
+    rawMesh = ndMesh(context, object)
+    rawMesh.MapArmatures(rawMesh)
+    
+    rawMesh.SaveXml(xmlRoot)
+    #SaveNodeDataRecurse(context, xmlRoot, object, matrix)
     
     # 4. Create an ElementTree object from the root element
     tree = ET.ElementTree(xmlRoot)
     
-    # 5. Write the tree to an XML file
+    # 5. Write the tree to a XML file
     ET.indent(xmlRoot)
     tree.write(filepath, encoding="utf-8", xml_declaration=True)
-    
-    # 6. (debug) Print the XML to the console
-    #print(ET.tostring(xmlRoot, encoding="utf-8").decode())
     
     return {'FINISHED'}
 
@@ -103,6 +169,7 @@ def SaveNodeDataTransfrom(context, xmlNode, blenderNode, rotation):
     matrix = rotation @ blenderNode.matrix_basis @ transposeRotation
     
     location, quaternion, scale = matrix.decompose()
+    
     eulers = quaternion.to_euler('XYZ')
     degrees = mathutils.Vector((math.degrees(eulers.x), math.degrees(eulers.y), math.degrees(eulers.z)))
     
