136c08bbb41d266fa41a1263c3d6f4c77d95a95a
NewtonDynamics <jerezjulio0@gmail.com>
2025-11-05 07:34:31 -0800

    for internal usage by skeleton solver block Cholesky  (wip)

--- newton-4.00/sdk/dCore/ndMatrix.h
+++ newton-4.00/sdk/dCore/ndMatrix.h
@@ -63,6 +63,15 @@ class ndMatrix : public ndClassAlloc
 
 	ndVector& operator[] (ndInt32 i);
 	const ndVector& operator[] (ndInt32 i) const;
+
+	inline ndMatrix Multiply(const ndMatrix& B) const
+	{
+		return ndMatrix(
+			B.m_front * m_front.BroadcastX() + B.m_up * m_front.BroadcastY() + B.m_right * m_front.BroadcastZ() + B.m_posit * m_front.BroadcastW(),
+			B.m_front * m_up.BroadcastX() + B.m_up * m_up.BroadcastY() + B.m_right * m_up.BroadcastZ() + B.m_posit * m_up.BroadcastW(),
+			B.m_front * m_right.BroadcastX() + B.m_up * m_right.BroadcastY() + B.m_right * m_right.BroadcastZ() + B.m_posit * m_right.BroadcastW(),
+			B.m_front * m_posit.BroadcastX() + B.m_up * m_posit.BroadcastY() + B.m_right * m_posit.BroadcastZ() + B.m_posit * m_posit.BroadcastW());
+	}
 	
 	D_CORE_API ndMatrix Inverse() const;
 	D_CORE_API ndMatrix OrthoInverse() const;
