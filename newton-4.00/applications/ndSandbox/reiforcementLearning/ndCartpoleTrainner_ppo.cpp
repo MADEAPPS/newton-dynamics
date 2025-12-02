94a4d9c1c7223a61aa1aef681a3bc01b95e1fafe
NewtonDynamics <jerezjulio0@gmail.com>
2025-12-02 10:06:16 -0800

    Update ndCartpoleTrainner_ppo.cpp

--- newton-4.00/applications/ndSandbox/reiforcementLearning/ndCartpoleTrainner_ppo.cpp
+++ newton-4.00/applications/ndSandbox/reiforcementLearning/ndCartpoleTrainner_ppo.cpp
@@ -108,7 +108,7 @@ namespace ndCartpoleTrainer_ppo
 			,m_discountRewardFactor(0.99f)
 			,m_horizon(ndFloat32(1.0f) / (ndFloat32(1.0f) - m_discountRewardFactor))
 			,m_lastEpisode(0xfffffff)
-			,m_stopTraining(2 * 1000000)
+			,m_stopTraining(20 * 1000000)
 			,m_modelIsTrained(false)
 		{
 			char name[256];
@@ -138,7 +138,7 @@ namespace ndCartpoleTrainer_ppo
 			loader.m_mesh->m_matrix = loader.m_mesh->m_matrix * matrix;
 			
 			// create an articulated model
-			const ndInt32 numberOfAgents = 1;
+			const ndInt32 numberOfAgents = 10;
 			for (ndInt32 i = 0; i < numberOfAgents; ++i)
 			{
 				ndFloat32 x = ndFloat32(10.0f) * (ndRand() - ndFloat32(0.5f));
