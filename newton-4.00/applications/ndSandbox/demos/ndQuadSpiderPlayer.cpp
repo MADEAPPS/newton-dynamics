019c204d954089ebd5f261eea09f3ee79dffc4a3
NewtonDynamics <jerezjulio0@gmail.com>
2025-12-20 12:02:54 -0800

    Update ndQuadSpiderPlayer.cpp
    added soft contact hard limit.

--- newton-4.00/applications/ndSandbox/demos/ndQuadSpiderPlayer.cpp
+++ newton-4.00/applications/ndSandbox/demos/ndQuadSpiderPlayer.cpp
@@ -498,6 +498,8 @@ namespace ndQuadSpiderPlayer
 			const ndMatrix contactAxis(ndRollMatrix(ndFloat32(90.0f) * ndDegreeToRad) * contactMatrix);
 			ndSharedPtr<ndJointBilateralConstraint> softContact(new ndJointSlider(contactAxis, contact->GetAsBodyKinematic(), heelBody->GetAsBodyKinematic()));
 			model->AddLimb(heelNode, contact, softContact);
+			((ndJointSlider*)*softContact)->SetLimitState(true);
+			((ndJointSlider*)*softContact)->SetLimits(-ndFloat32(0.05f), ndFloat32(0.05f));
 			((ndJointSlider*)*softContact)->SetAsSpringDamper(0.002f, 2000.0f, 100.0f);
 			
 			// save leg info
